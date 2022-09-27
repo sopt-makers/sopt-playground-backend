1bee53ffa4152157011f6dc8b24f10d20f664ccf
bluayer <ijacsong98@gmail.com>
2022-09-27 10:34:42 +0900

    feat: init project

---
+++ src/test/java/org/sopt/makers/internal/InternalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.makers.internal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
