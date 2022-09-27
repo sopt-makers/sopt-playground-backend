6e384b26529af24373c5223144cd38724809e5a1
bluayer <ijacsong98@gmail.com>
2022-09-27 11:48:23 +0900

    feat: service 생성

---
+++ src/main/java/org/sopt/makers/internal/dto/MemberSaveRequest.java
@@ -0,0 +1,3 @@
+package org.sopt.makers.internal.dto;
+
+public record MemberSaveRequest (String name, Integer generation) {}
---
+++ src/main/java/org/sopt/makers/internal/service/MemberService.java
@@ -0,0 +1,31 @@
+package org.sopt.makers.internal.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.makers.internal.domain.Member;
+import org.sopt.makers.internal.dto.MemberSaveRequest;
+import org.sopt.makers.internal.exception.NotFoundDBEntityException;
+import org.sopt.makers.internal.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member getMemberById (Long id) {
+        return memberRepository.findById(id).orElseThrow(() -> new NotFoundDBEntityException("Member"));
+    }
+
+    public Member createMember (MemberSaveRequest request) {
+        return memberRepository.save(Member.builder()
+                .name(request.name())
+                .generation(request.generation())
+                .build()
+        );
+    }
+
+    public Member getMemberByName (String name) {
+        return memberRepository.findByName(name).orElseThrow(() -> new NotFoundDBEntityException("Member"));
+    }
+
+}
