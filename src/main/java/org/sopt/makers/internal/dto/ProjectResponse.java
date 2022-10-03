741ca87377b5df7bd09dd4c82893f85f292c5cf4
Jungwoo Song <37579681+bluayer@users.noreply.github.com>
2022-10-03 20:45:56 +0900

    feat: making dto for read, save, update

---
+++ src/main/java/org/sopt/makers/internal/dto/ProjectResponse.java
@@ -0,0 +1,37 @@
+package org.sopt.makers.internal.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record ProjectResponse(
+        Long id,
+        String name,
+        Long writerId,
+        Integer generation,
+        String category,
+        LocalDate startAt,
+        LocalDate endAt,
+        String[] serviceType,
+        Boolean isAvailable,
+        Boolean isFounding,
+        String summary,
+        String detail,
+        String logoImage,
+        String thumbnailImage,
+        String[] images,
+        List<ProjectMemberResponse> member,
+        List<ProjectLinkResponse> link
+) {
+    public record ProjectMemberResponse(
+            Long memberId,
+            String memberRole,
+            String memberDesc,
+            Boolean isTeamMember
+    ){}
+
+    public record ProjectLinkResponse(
+            Long linkId,
+            String linkTitle,
+            String linkUrl
+    ){}
+}
