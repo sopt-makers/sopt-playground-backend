bbccc55c19679b76594e724230ee41137a27b4e7
Jungwoo Song <37579681+bluayer@users.noreply.github.com>
2022-10-03 21:57:03 +0900

    feat: repositories for entities

--- src/main/java/org/sopt/makers/internal/repository/ProjectRepository.java
+++ src/main/java/org/sopt/makers/internal/repository/ProjectRepository.java
@@ -1,4 +1,7 @@
 package org.sopt.makers.internal.repository;
 
-public interface ProjectRepository {
+import org.sopt.makers.internal.domain.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
