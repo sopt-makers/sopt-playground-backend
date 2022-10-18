d0f52a77c3969290022a38e354fe748602daecdd
Jungwoo Song <37579681+bluayer@users.noreply.github.com>
2022-10-18 21:22:07 +0900

    feat: add auth controller

--- src/main/java/org/sopt/makers/internal/service/AuthService.java
+++ src/main/java/org/sopt/makers/internal/service/AuthService.java
@@ -6,7 +6,7 @@ import org.sopt.makers.internal.domain.EmailSender;
 import org.sopt.makers.internal.domain.FacebookTokenManager;
 import org.sopt.makers.internal.domain.InternalTokenManager;
 import org.sopt.makers.internal.domain.Member;
-import org.sopt.makers.internal.exception.ClientBadRequestException;
+import org.sopt.makers.internal.exception.FacebookAuthFailureException;
 import org.sopt.makers.internal.exception.WrongTokenException;
 import org.sopt.makers.internal.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,11 +30,11 @@ public class AuthService {
     public String authByFb (String code) {
         val fbAccessToken = fbTokenManager.getAccessTokenByCode(code, "auth");
         if (fbAccessToken == null) {
-            // TODO : throw idpFailed exception
+            throw new FacebookAuthFailureException("Facebook 인증에 실패했습니다.");
         }
         val fbUserInfo = fbTokenManager.getUserInfo(fbAccessToken);
         val member = memberRepository.findByAuthUserId(fbUserInfo.userId())
-                .orElseThrow(() -> new ClientBadRequestException("Invalid User"));
+                .orElseThrow(() -> new FacebookAuthFailureException("SOPT.org 회원이 아닙니다."));
 
         return tokenManager.createAuthToken(member.getId());
     }
@@ -43,11 +43,11 @@ public class AuthService {
         val registerTokenInfo = tokenManager.verifyRegisterToken(registerToken);
         val fbAccessToken = fbTokenManager.getAccessTokenByCode(code, "register");
         if (registerTokenInfo == null) throw new WrongTokenException("tokenInvalid");
-        if (fbAccessToken == null) throw new WrongTokenException("idpFailed");
+        if (fbAccessToken == null) throw new FacebookAuthFailureException("facebook 인증에 실패했습니다.");
 
         val member = memberRepository.findByEmail(registerTokenInfo)
                 .orElseThrow(() -> new EntityNotFoundException("Member email" + registerTokenInfo + " not found"));
-        if (member.getIsJoined()) return "alreadyTaken";
+        if (member.getIsJoined()) throw new FacebookAuthFailureException("이미 가입된 사용자입니다.");
         member.makeMemberJoin();
 
         return tokenManager.createAuthToken(member.getId());
@@ -58,7 +58,7 @@ public class AuthService {
         return memberRepository.findByEmail(registerTokenInfo);
     }
 
-    public String sendRegisterLinkByEmail(String email) throws MessagingException {
+    public String sendRegisterLinkByEmail(String email) {
         val optionalMember = memberRepository.findByEmail(email);
         if (optionalMember.isEmpty()) return "invalidEmail";
 
@@ -67,7 +67,12 @@ public class AuthService {
 
         val token = tokenManager.createRegisterToken(email);
         val html = emailSender.createRegisterEmailHtml(token, registerPageUri);
-        emailSender.sendEmail(email, "SOPT 회원 인증", html);
-        return "success";
+        try {
+            emailSender.sendEmail(email, "SOPT 회원 인증", html);
+            return "success";
+        } catch (MessagingException exception) {
+            exception.printStackTrace();
+            return "cannotSendEmail";
+        }
     }
 }
