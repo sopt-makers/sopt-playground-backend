34448c665d9e5f8d94b76aeb9b1dbbd78a9cb8ee
bluayer <ijacsong98@gmail.com>
2022-10-20 11:08:59 +0900

    feat: security config 추가

---
+++ src/main/java/org/sopt/makers/internal/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package org.sopt.makers.internal.config;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.makers.internal.controller.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.antMatcher("/**")
+                .httpBasic().disable()
+                .formLogin().disable()
+                .cors().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeHttpRequests()
+                    .antMatchers("/v3/api-docs/**", "/swagger-ui.html", "/webjars/swagger-ui/**", "/swagger-ui/**").permitAll()
+                .and()
+                    .authorizeHttpRequests()
+                        .antMatchers("/api/v1/**").hasAuthority("Member")
+                .and()
+                    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
