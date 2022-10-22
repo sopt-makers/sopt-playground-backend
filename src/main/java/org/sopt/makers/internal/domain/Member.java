5f3a6c41c595683c9c35858529c666370cba0517
bluayer <ijacsong98@gmail.com>
2022-10-23 08:03:25 +0900

    feat: create member link for profile

--- src/main/java/org/sopt/makers/internal/domain/Member.java
+++ src/main/java/org/sopt/makers/internal/domain/Member.java
@@ -3,6 +3,7 @@ package org.sopt.makers.internal.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -27,4 +28,37 @@ public class Member {
 
     @Column(name = "generation")
     private Integer generation;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column
+    private LocalDate birthday;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String address;
+
+    @Column
+    private String university;
+
+    @Column
+    private String major;
+
+    @Column
+    private String introduction;
+
+    @Column
+    private String skill;
+
+    @Column(name = "open_to_work")
+    private Boolean openToWork;
+
+    @Column(name = "open_to_side_project")
+    private Boolean openToSideProject;
+
+    @Column(name = "allow_official")
+    private Boolean allowOfficial;
 }
