9f9d5138f37ce070eacf7c6ed22f20199a673dec
bluayer <ijacsong98@gmail.com>
2022-10-24 22:33:58 +0900

    feat: open cors

--- src/main/java/org/sopt/makers/internal/config/SecurityConfig.java
+++ src/main/java/org/sopt/makers/internal/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.sopt.makers.internal.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.sopt.makers.internal.controller.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,8 +28,9 @@ public class SecurityConfig {
         return http.antMatcher("/**")
                 .httpBasic().disable()
                 .formLogin().disable()
-                .cors().disable()
                 .csrf().disable()
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                     .authorizeHttpRequests()
@@ -34,5 +41,19 @@ public class SecurityConfig {
                 .and()
                     .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        val configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(false);
+
+        val source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
