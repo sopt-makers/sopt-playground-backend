7dc6c3a0b48c0d344a793307533a8bf8211f01a0
bluayer <ijacsong98@gmail.com>
2022-10-26 11:02:46 +0900

    feat: member에 update logic 추가, entity 매핑 추가

--- src/main/java/org/sopt/makers/internal/domain/Member.java
+++ src/main/java/org/sopt/makers/internal/domain/Member.java
@@ -4,6 +4,9 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -47,6 +50,14 @@ public class Member {
     @Column
     private String major;
 
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    private List<MemberSoptActivity> activities = new ArrayList<>();
+
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    private List<MemberLink> links = new ArrayList<>();
+
     @Column
     private String introduction;
 
@@ -61,4 +72,38 @@ public class Member {
 
     @Column(name = "allow_official")
     private Boolean allowOfficial;
+
+    public void saveMemberProfile(
+            String name,
+            String profileImage,
+            LocalDate birthday,
+            String phone,
+            String email,
+            String address,
+            String university,
+            String major,
+            String introduction,
+            String skill,
+            Boolean openToWork,
+            Boolean openToSideProject,
+            Boolean allowOfficial,
+            List<MemberSoptActivity> activities,
+            List<MemberLink> links
+    ) {
+        this.name = Objects.requireNonNullElse(name, this.name);
+        this.profileImage = Objects.requireNonNullElse(profileImage, this.profileImage);
+        this.birthday = Objects.requireNonNullElse(birthday, this.birthday);
+        this.phone = Objects.requireNonNullElse(phone, this.phone);
+        this.email =Objects.requireNonNullElse(email, this.email);
+        this.address = Objects.requireNonNullElse(address, this.address);
+        this.university = Objects.requireNonNullElse(university, this.university);
+        this.major = Objects.requireNonNullElse(major, this.major);
+        this.introduction = Objects.requireNonNullElse(introduction, this.introduction);
+        this.skill = Objects.requireNonNullElse(skill, this.skill);
+        this.openToWork = Objects.requireNonNullElse(openToWork, this.openToWork);
+        this.openToSideProject = Objects.requireNonNullElse(openToSideProject, this.openToSideProject);
+        this.allowOfficial = Objects.requireNonNullElse(allowOfficial, this.allowOfficial);
+        this.activities = activities;
+        this.links = links;
+    }
 }
