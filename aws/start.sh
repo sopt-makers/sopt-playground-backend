7a97ce3cb8bb30d5b9f46474a471f939ab71645b
bluayer <ijacsong98@gmail.com>
2022-10-26 17:19:58 +0900

    hotfix: remove stop.sh

--- aws/start.sh
+++ aws/start.sh
@@ -4,5 +4,8 @@ cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
+kill $(ps aux | grep java | grep -v grep | awk '{print $2}')
+echo "java stopped"
+
 echo "> $JAR_PATH 배포" #3
 nohup java -jar -Dspring.profiles.active=dev $REPOSITORY/build/libs/internal-0.0.1-SNAPSHOT.jar >nohup.out 2>&1 </dev/null &
--- aws/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kill $(ps aux | grep java | grep -v grep | awk '{print $2}')
-echo "java stopped"
