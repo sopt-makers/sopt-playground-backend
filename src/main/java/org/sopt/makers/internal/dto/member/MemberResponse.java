dcc2af58f951d86b19c964de7d4fcaf5a8362f22
bluayer <ijacsong98@gmail.com>
2022-11-02 22:22:31 +0900

    feat: hasProfile at response

--- src/main/java/org/sopt/makers/internal/dto/member/MemberResponse.java
+++ src/main/java/org/sopt/makers/internal/dto/member/MemberResponse.java
@@ -3,5 +3,6 @@ package org.sopt.makers.internal.dto.member;
 public record MemberResponse(
     Long id,
     String name,
-    Integer generation
+    Integer generation,
+    Boolean hasProfile
 ) {}
