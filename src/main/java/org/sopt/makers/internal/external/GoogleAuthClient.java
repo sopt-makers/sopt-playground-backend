20c019c5275fae2aa4abf1a16cadd299746ba728
bluayer <ijacsong98@gmail.com>
2022-12-05 06:00:02 +0900

    fix: change http method

--- src/main/java/org/sopt/makers/internal/external/GoogleAuthClient.java
+++ src/main/java/org/sopt/makers/internal/external/GoogleAuthClient.java
@@ -1,14 +1,13 @@
 package org.sopt.makers.internal.external;
 
-import org.sopt.makers.internal.dto.auth.FacebookAccessTokenResponse;
 import org.sopt.makers.internal.dto.auth.GoogleAccessTokenResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "googleAuth", url = "https://oauth2.googleapis.com")
 public interface GoogleAuthClient {
-    @GetMapping("/token")
+    @PostMapping("/token")
     GoogleAccessTokenResponse getAccessToken(
             @RequestParam("client_id") String clientAppId,
             @RequestParam("client_secret") String clientSecret,
