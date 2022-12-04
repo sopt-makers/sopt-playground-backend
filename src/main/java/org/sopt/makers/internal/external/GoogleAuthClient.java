4c5804abb408211068da2c3412d2d53012078d63
bluayer <ijacsong98@gmail.com>
2022-12-05 05:44:32 +0900

    feat: making auth client for google

---
+++ src/main/java/org/sopt/makers/internal/external/GoogleAuthClient.java
@@ -0,0 +1,19 @@
+package org.sopt.makers.internal.external;
+
+import org.sopt.makers.internal.dto.auth.FacebookAccessTokenResponse;
+import org.sopt.makers.internal.dto.auth.GoogleAccessTokenResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "googleAuth", url = "https://oauth2.googleapis.com")
+public interface GoogleAuthClient {
+    @GetMapping("/token")
+    GoogleAccessTokenResponse getAccessToken(
+            @RequestParam("client_id") String clientAppId,
+            @RequestParam("client_secret") String clientSecret,
+            @RequestParam("code") String code,
+            @RequestParam("grant_type") String grantType,
+            @RequestParam("redirect_uri") String redirectUri
+    );
+}
