668ecd87d8b79672db6b8c976ad5f7a4b15cbf9c
bluayer <ijacsong98@gmail.com>
2022-12-05 05:47:15 +0900

    refac: remove unnecessary dependencies

--- src/main/java/org/sopt/makers/internal/controller/HealthCheckController.java
+++ src/main/java/org/sopt/makers/internal/controller/HealthCheckController.java
@@ -1,13 +1,9 @@
 package org.sopt.makers.internal.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.val;
-import org.sopt.makers.internal.dto.auth.AccessTokenResponse;
-import org.sopt.makers.internal.dto.auth.AuthByFacebookRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
