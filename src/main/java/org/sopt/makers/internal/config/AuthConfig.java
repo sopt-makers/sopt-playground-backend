4ce9dcade8df52c742f21161737f3fdcd396890a
bluayer <ijacsong98@gmail.com>
2022-12-05 05:46:56 +0900

    feat: change name of secret key for jwt

--- src/main/java/org/sopt/makers/internal/config/AuthConfig.java
+++ src/main/java/org/sopt/makers/internal/config/AuthConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AuthConfig {
     @Value("${jwt.secret}")
-    private String fbSecretKey;
+    private String jwtSecretKey;
 
     @Value("${oauth.fb.redirect.auth}")
     private String fbRedirectUriAuth;
