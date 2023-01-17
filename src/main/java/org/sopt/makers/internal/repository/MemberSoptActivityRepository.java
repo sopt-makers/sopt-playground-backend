c39d4a6c1a8a3bc0a716c1db58f194bba668ee71
bluayer <ijacsong98@gmail.com>
2023-01-17 16:14:30 +0900

    feat: add repository function for get activities

--- src/main/java/org/sopt/makers/internal/repository/MemberSoptActivityRepository.java
+++ src/main/java/org/sopt/makers/internal/repository/MemberSoptActivityRepository.java
@@ -3,8 +3,11 @@ package org.sopt.makers.internal.repository;
 import org.sopt.makers.internal.domain.MemberSoptActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberSoptActivityRepository extends JpaRepository<MemberSoptActivity, Long> {
     Optional<MemberSoptActivity> findByIdAndMemberId(Long id, Long memberId);
+
+    List<MemberSoptActivity> findAllByMemberIdIn(List<Long> memberIds);
 }
