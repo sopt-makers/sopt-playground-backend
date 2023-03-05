600ef250c7be1331a69156c6de54d17e108c8d9d
yujindonut <bbb3631@naver.com>
2023-03-05 18:13:10 +0900

    chore: 2기 멤버 추가

--- src/main/java/org/sopt/makers/internal/service/MemberService.java
+++ src/main/java/org/sopt/makers/internal/service/MemberService.java
@@ -83,7 +83,9 @@ public class MemberService {
     public List<Member> getAllMakersMemberProfiles() {
         val makersMembers = List.of(
                 1L, 44L, 23L, 31L, 8L, 30L, 40L, 46L, 26L, 60L, 39L, 6L, 9L, 7L, 2L, 3L, 29L,
-                5L, 38L, 37L, 13L, 28L, 36L, 58L, 173L, 32L, 43L, 188L, 59L, 34L, 21L, 33L, 22L, 35L, 45L
+                5L, 38L, 37L, 13L, 28L, 36L, 58L, 173L, 32L, 43L, 188L, 59L, 34L, 21L, 33L, 22L, 35L, 45L,
+                186L, 227L, 264L, 4L, 51L, 187L, 128L, 64L, 99L, 10L, 66L, 260L, 72L, 265L, 74L, 251L,
+                115L, 258L, 112L, 205L, 238L
         );
         return memberRepository.findAllByHasProfileTrueAndIdIn(makersMembers);
     }
