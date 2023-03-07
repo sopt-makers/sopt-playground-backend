496257669b6b009f5d0bd87ca6ef7a7310c89931
yujindonut <bbb3631@naver.com>
2023-03-08 00:58:50 +0900

    chore: 불필요한 의존성 제거

--- src/main/java/org/sopt/makers/internal/controller/MemberController.java
+++ src/main/java/org/sopt/makers/internal/controller/MemberController.java
@@ -16,12 +16,10 @@ import org.sopt.makers.internal.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
