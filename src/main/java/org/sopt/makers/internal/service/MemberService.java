0cb1c8112f49d74215f086e38bd8e4b20c8318d3
yujindonut <bbb3631@naver.com>
2023-03-17 00:49:14 +0900

    chore: 솝트메이커스 멤버 리스트 id 추가
    솝트메이커스 멤버가 추가됨에 따라 id를 추가하였습니다

--- src/main/java/org/sopt/makers/internal/service/MemberService.java
+++ src/main/java/org/sopt/makers/internal/service/MemberService.java
@@ -85,7 +85,7 @@ public class MemberService {
                 1L, 44L, 23L, 31L, 8L, 30L, 40L, 46L, 26L, 60L, 39L, 6L, 9L, 7L, 2L, 3L, 29L,
                 5L, 38L, 37L, 13L, 28L, 36L, 58L, 173L, 32L, 43L, 188L, 59L, 34L, 21L, 33L, 22L, 35L, 45L,
                 186L, 227L, 264L, 4L, 51L, 187L, 128L, 64L, 99L, 10L, 66L, 260L, 72L, 265L, 74L, 251L,
-                115L, 258L, 112L, 205L, 238L
+                115L, 258L, 112L, 205L, 238L, 259L, 281L, 285L, 286L, 283L, 282L
         );
         return memberRepository.findAllByHasProfileTrueAndIdIn(makersMembers);
     }
