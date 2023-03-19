a5d045a23fb7b5ae607bf8b46470c17a2049cc8c
JungWoo Song <37579681+bluayer@users.noreply.github.com>
2023-03-19 19:46:20 +0900

    feat: 보안 이슈로 토큰 기간 감소시킴
    - 10일은 너무 길어서 8시간으로 변경

--- src/main/java/org/sopt/makers/internal/domain/InternalTokenManager.java
+++ src/main/java/org/sopt/makers/internal/domain/InternalTokenManager.java
@@ -34,7 +34,7 @@ public class InternalTokenManager {
         val secretKeyBytes = DatatypeConverter.parseBase64Binary(authConfig.getJwtSecretKey());
         val signingKey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName());
         val exp = new Date().toInstant().atZone(KST)
-                .toLocalDateTime().plusDays(10).atZone(KST).toInstant();
+                .toLocalDateTime().plusHours(8).atZone(KST).toInstant();
         return Jwts.builder()
                 .setSubject(Long.toString(userId))
                 .setExpiration(Date.from(exp))
