b328033f07754ec222ac5f266e6b80cffe4b6f50
JungWoo Song <37579681+bluayer@users.noreply.github.com>
2023-03-27 21:09:05 +0900

    feat: mapper에 함수 추가

--- src/main/java/org/sopt/makers/internal/controller/InternalOpenApiController.java
+++ src/main/java/org/sopt/makers/internal/controller/InternalOpenApiController.java
@@ -8,12 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.sopt.makers.internal.domain.InternalMemberDetails;
 import org.sopt.makers.internal.domain.Project;
-import org.sopt.makers.internal.dto.member.MemberAllProfileResponse;
-import org.sopt.makers.internal.dto.member.MemberProfileSpecificResponse;
-import org.sopt.makers.internal.dto.member.MemberResponse;
-import org.sopt.makers.internal.dto.project.ProjectDetailResponse;
+import org.sopt.makers.internal.dto.internal.*;
 import org.sopt.makers.internal.dto.project.ProjectLinkDao;
-import org.sopt.makers.internal.dto.project.ProjectResponse;
 import org.sopt.makers.internal.mapper.MemberMapper;
 import org.sopt.makers.internal.mapper.ProjectResponseMapper;
 import org.sopt.makers.internal.service.InternalApiService;
@@ -42,40 +38,40 @@ public class InternalOpenApiController {
 
     @Operation(summary = "Project id로 조회 API")
     @GetMapping("/projects/{id}")
-    public ResponseEntity<ProjectDetailResponse> getProject (@PathVariable Long id) {
+    public ResponseEntity<InternalProjectDetailResponse> getProject (@PathVariable Long id) {
         val projectMembers = internalApiService.fetchById(id);
         val projectLinks = internalApiService.fetchLinksById(id);
-        val response = projectMapper.toProjectDetailResponse(projectMembers, projectLinks);
+        val response = projectMapper.toInternalProjectDetailResponse(projectMembers, projectLinks);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @Operation(summary = "Project 전체 조회 API")
     @GetMapping("/projects")
-    public ResponseEntity<List<ProjectResponse>> getProjects () {
+    public ResponseEntity<List<InternalProjectResponse>> getProjects () {
         val projectMap = internalApiService.fetchAll().stream()
                 .collect(Collectors.toMap(Project::getId, Function.identity()));
         val projectLinkMap = internalApiService.fetchAllLinks().stream()
                 .collect(Collectors.groupingBy(ProjectLinkDao::id, Collectors.toList()));
         val projectIds = projectMap.keySet();
         val responses = projectIds.stream()
-                .map(id -> projectMapper.toProjectResponse(projectMap.get(id), projectLinkMap.getOrDefault(id, List.of())))
+                .map(id -> projectMapper.toInternalProjectResponse(projectMap.get(id), projectLinkMap.getOrDefault(id, List.of())))
                 .collect(Collectors.toList());
         return ResponseEntity.status(HttpStatus.OK).body(responses);
     }
 
     @Operation(summary = "자신의 토큰으로 조회 API")
     @GetMapping("/members/me")
-    public ResponseEntity<MemberResponse> getMyInformation (
+    public ResponseEntity<InternalMemberResponse> getMyInformation (
             @Parameter(hidden = true) @AuthenticationPrincipal InternalMemberDetails memberDetails
     ) {
         val member = internalApiService.getMemberById(memberDetails.getId());
-        val response = memberMapper.toResponse(member);
+        val response = memberMapper.toInternalResponse(member);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @Operation(summary = "자신의 토큰으로 프로필 조회 API")
     @GetMapping("/members/profile/me")
-    public ResponseEntity<MemberProfileSpecificResponse> getMyProfile (
+    public ResponseEntity<InternalMemberProfileSpecificResponse> getMyProfile (
             @Parameter(hidden = true) @AuthenticationPrincipal InternalMemberDetails memberDetails
     ) {
         val id = memberDetails.getId();
@@ -86,24 +82,24 @@ public class InternalOpenApiController {
                 memberProfileProjects
         );
         val activityResponses = activityMap.entrySet().stream().map(entry ->
-                new MemberProfileSpecificResponse.MemberActivityResponse(entry.getKey(), entry.getValue())
+                new InternalMemberProfileSpecificResponse.MemberActivityResponse(entry.getKey(), entry.getValue())
         ).collect(Collectors.toList());
         val isMine = Objects.equals(member.getId(), memberDetails.getId());
-        val response = memberMapper.toProfileSpecificResponse(
+        val response = memberMapper.toInternalProfileSpecificResponse(
                 member, isMine, memberProfileProjects, activityResponses
         );
         sortProfileCareer(response);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    private void sortProfileCareer (MemberProfileSpecificResponse response) {
+    private void sortProfileCareer (InternalMemberProfileSpecificResponse response) {
         response.careers().sort((a, b) -> {
             val formatter = DateTimeFormatter.ofPattern("yyyy-MM");
             val start = YearMonth.parse(a.startDate(), formatter);
             val end = YearMonth.parse(b.startDate(), formatter);
             return end.compareTo(start);
         });
-        MemberProfileSpecificResponse.MemberCareerResponse currentCareer = null;
+        InternalMemberProfileSpecificResponse.MemberCareerResponse currentCareer = null;
         int index = 0;
         for (val career: response.careers()) {
             if (career.isCurrent()) {
@@ -127,7 +123,7 @@ public class InternalOpenApiController {
                     """
     )
     @GetMapping("/profile")
-    public ResponseEntity<MemberAllProfileResponse> getUserProfiles (
+    public ResponseEntity<InternalMemberAllProfileResponse> getUserProfiles (
             @RequestParam(required = false, name = "filter") Integer filter,
             @RequestParam(required = false, name = "limit") Integer limit,
             @RequestParam(required = false, name = "cursor") Integer cursor,
@@ -135,12 +131,10 @@ public class InternalOpenApiController {
             @RequestParam(required = false, name = "generation") Integer generation
     ) {
         val members = limit == null ? internalApiService.getMemberProfiles(filter, limit, cursor, name, generation) : internalApiService.getMemberProfiles(filter, limit + 1, cursor, name, generation);
-        val memberList = members.stream().map(memberMapper::toProfileResponse).collect(Collectors.toList());
+        val memberList = members.stream().map(memberMapper::toInternalProfileResponse).collect(Collectors.toList());
         val hasNextMember = (limit != null && memberList.size() > limit);
         if (hasNextMember) memberList.remove(members.size() - 1);
-        val response = new MemberAllProfileResponse(memberList, hasNextMember);
+        val response = new InternalMemberAllProfileResponse(memberList, hasNextMember);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
-
-
 }
