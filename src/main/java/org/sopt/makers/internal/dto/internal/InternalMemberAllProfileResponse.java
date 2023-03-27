868e28e28facab89875b9f5468a1cf2fc0dab1b8
JungWoo Song <37579681+bluayer@users.noreply.github.com>
2023-03-27 21:10:34 +0900

    chore: 불필요한 의존성 제거

--- src/main/java/org/sopt/makers/internal/dto/internal/InternalMemberAllProfileResponse.java
+++ src/main/java/org/sopt/makers/internal/dto/internal/InternalMemberAllProfileResponse.java
@@ -1,7 +1,5 @@
 package org.sopt.makers.internal.dto.internal;
 
-import org.sopt.makers.internal.dto.member.MemberProfileResponse;
-
 import java.util.List;
 
 public record InternalMemberAllProfileResponse(
