4b61b6559a9df4c5b4413cad0da00d2657454ae1
한유진 <78431728+yujindonut@users.noreply.github.com>
2023-04-01 23:49:47 +0900

    멤버 프로필 활동정보 null 처리 수정 (#126)
    * chore: request null handling
    - null일 경우, object.equals('해당 없음')에서 오류 발생
    - null 검사를 먼저하기 수정
    
    * chore: response null, '해당 없음' 처리
    - DB 데이터가 null, '해당 없음'경우 null로 반환되게 처리
    
    * chore: null error 수정
    - null이 들어올 경우 .equals() 메소드 오류로 null 조건을 포함시켰습니다.
    
    * chore: profile Response 수정
    - 코드리뷰 반영해 코드 생성..
    
    * feat: ActivityTeam Enum 생성
    
    * chore: Request 요청시, enum으로 handling
    
    * feat: Predicate 메소드 생성
    - 메소드를 통해 조건문에 대한 가독성을높였습니다..
    
    * chore: mapper Response 수정
    
    * chore: ActivityTeam Enum 메소드 null handling
    
    * chore: 멤버 profile 조회시, mapper response 수정
    
    * chore: val 수정

---
+++ src/main/java/org/sopt/makers/internal/domain/ActivityTeam.java
@@ -0,0 +1,22 @@
+package org.sopt.makers.internal.domain;
+
+import java.util.Arrays;
+import lombok.val;
+
+public enum ActivityTeam {
+    EMPTY(null),
+    NO_TEAM("해당 없음"),
+    MEDIA_TEAM("미디어팀"),
+    OPERATION_TEAM("운영팀");
+
+    final String teamName;
+    ActivityTeam(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public static boolean hasActivityTeam(String team) {
+        val rightTeam = Arrays.stream(ActivityTeam.values())
+                .filter(v -> (team == null || team.equals(v.teamName))).findAny();
+        return rightTeam.isPresent();
+    }
+}
