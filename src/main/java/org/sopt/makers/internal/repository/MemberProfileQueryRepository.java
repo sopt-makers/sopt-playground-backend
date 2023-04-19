dd1d264af144561d32a0fed3823f00603874c02c
yujindonut <bbb3631@naver.com>
2023-04-19 13:25:28 +0900

    feat: add Boolean Expression and OrderSpecifier

--- src/main/java/org/sopt/makers/internal/repository/MemberProfileQueryRepository.java
+++ src/main/java/org/sopt/makers/internal/repository/MemberProfileQueryRepository.java
@@ -1,5 +1,6 @@
 package org.sopt.makers.internal.repository;
 
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -50,7 +51,33 @@ public class MemberProfileQueryRepository {
     private BooleanExpression checkMemberHasProfile() {
         return QMember.member.hasProfile.eq(true);
     }
-    public List<Member> findAllLimitedMemberProfile(String part, Integer limit, Integer cursor, String name, Integer generation) {
+    private BooleanExpression checkMemberMbti(String mbti) {
+        if(mbti == null) return null;
+        return QMember.member.mbti.eq(mbti);
+    }
+    private BooleanExpression checkMemberSojuCapactiy(Double sojuCapactiy) {
+        if(sojuCapactiy == null) return null;
+        return QMember.member.sojuCapacity.eq(sojuCapactiy);
+    }
+    private OrderSpecifier getOrderByDropDownNumber(Integer orderByDropDown) {
+        if (orderByDropDown == null) return QMember.member.id.desc();
+        switch (orderByDropDown) {
+            case 1 -> { // 최근에 등록했순 (프로필 등록 최근순)
+                return QMember.member.id.desc();
+            }
+            case 2 -> { // 예전에 등록했순 (프로필 등록 오래된순)
+                return QMember.member.id.asc();
+            }
+            case 3 -> { // 최근에 활동했순 (최근 기수순)
+                return QMemberSoptActivity.memberSoptActivity.generation.max().desc();
+            }
+            default -> { // 예전에 활동했순 (오래된 기수순)
+                return QMemberSoptActivity.memberSoptActivity.generation.min().asc();
+            }
+        }
+    }
+    public List<Member> findAllLimitedMemberProfile(String part, Integer limit, Integer cursor, String name,
+                        Integer generation, Double sojuCapactiy, Integer orderByDropDown, String mbti) {
         val member = QMember.member;
         val activities = QMemberSoptActivity.memberSoptActivity;
         return queryFactory.selectFrom(member)
