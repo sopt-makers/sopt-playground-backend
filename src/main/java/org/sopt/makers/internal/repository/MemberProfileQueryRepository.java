1dc330702c44853f428e04a78586628d5646e750
yujindonut <bbb3631@naver.com>
2023-04-24 00:29:19 +0900

    feat: 임원진 필터링 추가 - 같은 필드 조회시, and 조건 괄호 문제로 분기 처리 메소드를 생성하게 됨

--- src/main/java/org/sopt/makers/internal/repository/MemberProfileQueryRepository.java
+++ src/main/java/org/sopt/makers/internal/repository/MemberProfileQueryRepository.java
@@ -42,6 +42,13 @@ public class MemberProfileQueryRepository {
         if(part == null) return null;
         return QMemberSoptActivity.memberSoptActivity.part.contains(part);
     }
+
+    private BooleanExpression checkActivityContainsPartAndGeneration(String part, Integer generation) {
+        if (part == null) return null;
+        return QMemberSoptActivity.memberSoptActivity.part.contains(part)
+                .and(QMemberSoptActivity.memberSoptActivity.generation.eq(generation));
+    }
+
     private BooleanExpression checkIdGtThanCursor(Integer cursor) {
         if(cursor == null) return null;
         return QMember.member.id.gt(cursor);
@@ -83,15 +90,160 @@ public class MemberProfileQueryRepository {
             }
         }
     }
+
+    private Predicate checkMemberGenerationAndTeamAndPart(Integer generation, String team, String part) {
+        if(generation == null) {
+            if(part == null && team == null) return null;
+            else if(part == null && team != null) return checkMemberBelongToTeam(team);
+            else if(team == null && part != null) return checkActivityContainsPart(part);
+            else return checkMemberBelongToTeamAndPart(team, part);
+        }
+        else {
+            if(team == null && part == null) return checkActivityContainsGeneration(generation);
+            else if(team == null && part != null) return checkActivityContainsPartAndGeneration(part, generation);
+            else if(part == null) return checkMemberBelongToTeamAndGeneration(team, generation);
+            else return checkMemberBelongToTeamAndPartAndGeneration(team, part, generation);
+        }
+    }
+
+    private BooleanExpression checkMemberBelongToTeamAndPart(String team, String part) {
+        if (team == null || part == null) return null;
+        switch (team) {
+            case "임원진" -> {
+                return QMemberSoptActivity.memberSoptActivity.part.contains(part)
+                        .and(QMemberSoptActivity.memberSoptActivity.part.contains("메이커스 리드")
+                                .or(QMemberSoptActivity.memberSoptActivity.part.contains("총무"))
+                                .or(QMemberSoptActivity.memberSoptActivity.part.contains("장"))); // 회장, 부회장, ~파트장, 운영 팀장, 미디어 팀장
+            }
+            case "운영팀" -> {
+                return QMemberSoptActivity.memberSoptActivity.part.contains(part)
+                        .and(QMemberSoptActivity.memberSoptActivity.team.contains("운영팀")
+                                .or(QMemberSoptActivity.memberSoptActivity.part.contains("운영 팀장")));
+            }
+            case "미디어팀" -> {
+                return QMemberSoptActivity.memberSoptActivity.part.contains(part)
+                        .and(QMemberSoptActivity.memberSoptActivity.team.contains("미디어팀")
+                                .or(QMemberSoptActivity.memberSoptActivity.part.contains("미디어 팀장")));
+            }
+            case "메이커스" -> {
+                return QMemberSoptActivity.memberSoptActivity.part.contains(part)
+                        .and(QMember.member.id.in(1L, 44L, 23L, 31L, 8L, 30L, 40L, 46L, 26L, 60L, 39L, 6L, 9L, 7L, 2L, 3L, 29L,
+                                5L, 38L, 37L, 13L, 28L, 36L, 58L, 173L, 32L, 43L, 188L, 59L, 34L, 21L, 33L, 22L, 35L, 45L,
+                                186L, 227L, 264L, 4L, 51L, 187L, 128L, 64L, 99L, 10L, 66L, 260L, 72L, 265L, 78L, 251L,
+                                115L, 258L, 112L, 205L, 238L, 259L, 281L, 285L, 286L, 283L, 282L));
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
+    private BooleanExpression checkMemberBelongToTeamAndPartAndGeneration(String team, String part, Integer generation) {
+        if (team == null || part == null) return null;
+        switch (team) {
+            case "임원진" -> {
+                return QMemberSoptActivity.memberSoptActivity.generation.eq(generation)
+                        .and(QMemberSoptActivity.memberSoptActivity.part.contains(part)
+                        .and(QMemberSoptActivity.memberSoptActivity.part.contains("메이커스 리드"))
+                                .or(QMemberSoptActivity.memberSoptActivity.part.contains("총무")
+                                .or(QMemberSoptActivity.memberSoptActivity.part.contains("장")))); // 회장, 부회장, ~파트장, 운영 팀장, 미디어 팀장
+            }
+            case "운영팀" -> {
+                return QMemberSoptActivity.memberSoptActivity.generation.eq(generation)
+                        .and(QMemberSoptActivity.memberSoptActivity.part.contains(part)
+                        .and(QMemberSoptActivity.memberSoptActivity.team.contains("운영팀"))
+                                .or(QMemberSoptActivity.memberSoptActivity.part.contains("운영 팀장")));
+            }
+            case "미디어팀" -> {
+                return QMemberSoptActivity.memberSoptActivity.generation.eq(generation)
+                        .and(QMemberSoptActivity.memberSoptActivity.part.contains(part)
+                        .and(QMemberSoptActivity.memberSoptActivity.team.contains("미디어팀"))
+                                .or(QMemberSoptActivity.memberSoptActivity.part.contains("미디어 팀장")));
+            }
+            case "메이커스" -> {
+                return QMemberSoptActivity.memberSoptActivity.generation.eq(generation)
+                        .and(QMemberSoptActivity.memberSoptActivity.part.contains(part)
+                        .and(QMember.member.id.in(1L, 44L, 23L, 31L, 8L, 30L, 40L, 46L, 26L, 60L, 39L, 6L, 9L, 7L, 2L, 3L, 29L,
+                                5L, 38L, 37L, 13L, 28L, 36L, 58L, 173L, 32L, 43L, 188L, 59L, 34L, 21L, 33L, 22L, 35L, 45L,
+                                186L, 227L, 264L, 4L, 51L, 187L, 128L, 64L, 99L, 10L, 66L, 260L, 72L, 265L, 78L, 251L,
+                                115L, 258L, 112L, 205L, 238L, 259L, 281L, 285L, 286L, 283L, 282L)));
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
+    private BooleanExpression checkMemberBelongToTeam(String team) {
+        if (team == null) return null;
+        switch (team) {
+            case "임원진" -> {
+                return QMemberSoptActivity.memberSoptActivity.part.contains("메이커스 리드")
+                        .or(QMemberSoptActivity.memberSoptActivity.part.contains("총무"))
+                        .or(QMemberSoptActivity.memberSoptActivity.part.contains("장")); // 회장, 부회장, ~파트장, 운영 팀장, 미디어 팀장
+            }
+            case "운영팀" -> {
+                return QMemberSoptActivity.memberSoptActivity.team.contains("운영팀")
+                        .or(QMemberSoptActivity.memberSoptActivity.part.contains("운영 팀장"));
+            }
+            case "미디어팀" -> {
+                return QMemberSoptActivity.memberSoptActivity.team.contains("미디어팀")
+                        .or(QMemberSoptActivity.memberSoptActivity.part.contains("미디어 팀장"));
+            }
+            case "메이커스" -> {
+                return QMember.member.id.in(1L, 44L, 23L, 31L, 8L, 30L, 40L, 46L, 26L, 60L, 39L, 6L, 9L, 7L, 2L, 3L, 29L,
+                                5L, 38L, 37L, 13L, 28L, 36L, 58L, 173L, 32L, 43L, 188L, 59L, 34L, 21L, 33L, 22L, 35L, 45L,
+                                186L, 227L, 264L, 4L, 51L, 187L, 128L, 64L, 99L, 10L, 66L, 260L, 72L, 265L, 78L, 251L,
+                                115L, 258L, 112L, 205L, 238L, 259L, 281L, 285L, 286L, 283L, 282L);
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
+    private BooleanExpression checkMemberBelongToTeamAndGeneration(String team, Integer generation) {
+        if (team == null) return null;
+        System.out.println(team + " " + generation);
+        switch (team) {
+            case "임원진" -> {
+                return QMemberSoptActivity.memberSoptActivity.generation.eq(generation)
+                        .and(QMemberSoptActivity.memberSoptActivity.part.contains("메이커스 리드")
+                        .or(QMemberSoptActivity.memberSoptActivity.part.contains("총무"))
+                        .or(QMemberSoptActivity.memberSoptActivity.part.contains("장"))); // 회장, 부회장, ~파트장, 운영 팀장, 미디어 팀장
+            }
+            case "운영팀" -> {
+                return QMemberSoptActivity.memberSoptActivity.generation.eq(generation)
+                        .and(QMemberSoptActivity.memberSoptActivity.team.contains("운영팀")
+                        .or(QMemberSoptActivity.memberSoptActivity.part.contains("운영 팀장")));
+            }
+            case "미디어팀" -> {
+                return QMemberSoptActivity.memberSoptActivity.generation.eq(generation)
+                        .and(QMemberSoptActivity.memberSoptActivity.team.contains("미디어팀")
+                        .or(QMemberSoptActivity.memberSoptActivity.part.contains("미디어 팀장")));
+            }
+            case "메이커스" -> {
+                return QMemberSoptActivity.memberSoptActivity.generation.eq(generation)
+                        .and(QMember.member.id.in(1L, 44L, 23L, 31L, 8L, 30L, 40L, 46L, 26L, 60L, 39L, 6L, 9L, 7L, 2L, 3L, 29L,
+                        5L, 38L, 37L, 13L, 28L, 36L, 58L, 173L, 32L, 43L, 188L, 59L, 34L, 21L, 33L, 22L, 35L, 45L,
+                        186L, 227L, 264L, 4L, 51L, 187L, 128L, 64L, 99L, 10L, 66L, 260L, 72L, 265L, 78L, 251L,
+                        115L, 258L, 112L, 205L, 238L, 259L, 281L, 285L, 286L, 283L, 282L));
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
     public List<Member> findAllLimitedMemberProfile(String part, Integer limit, Integer cursor, String name,
-                        Integer generation, Double sojuCapactiy, Integer orderByDropDown, String mbti) {
+                        Integer generation, Double sojuCapactiy, Integer orderByDropDown, String mbti, String team) {
         val member = QMember.member;
         val activities = QMemberSoptActivity.memberSoptActivity;
         return queryFactory.selectFrom(member)
                 .innerJoin(member.activities, activities)
-                .where(checkMemberHasProfile(), checkActivityContainsPart(part), checkIdGtThanCursor(cursor),
-                        checkActivityContainsGeneration(generation), checkMemberContainsName(name),
-                        checkMemberMbti(mbti), checkMemberSojuCapactiy(sojuCapactiy))
+                .where(checkMemberHasProfile(), checkIdGtThanCursor(cursor),
+                        checkMemberGenerationAndTeamAndPart(generation, team, part),
+                        checkMemberContainsName(name), checkMemberMbti(mbti), checkMemberSojuCapactiy(sojuCapactiy))
                 .limit(limit)
                 .groupBy(member.id)
                 .orderBy(getOrderByDropDownNumber(orderByDropDown))
@@ -99,14 +251,14 @@ public class MemberProfileQueryRepository {
     }
 
     public List<Member> findAllMemberProfile(String part, Integer cursor, String name, Integer generation,
-                       Double sojuCapactiy, Integer orderByDropDown, String mbti) {
+                                             Double sojuCapactiy, Integer orderByDropDown, String mbti, String team) {
         val member = QMember.member;
         val activities = QMemberSoptActivity.memberSoptActivity;
         return queryFactory.selectFrom(member)
                 .innerJoin(member.activities, activities)
-                .where(checkMemberHasProfile(), checkActivityContainsPart(part), checkIdGtThanCursor(cursor),
-                        checkActivityContainsGeneration(generation), checkMemberContainsName(name),
-                        checkMemberMbti(mbti), checkMemberSojuCapactiy(sojuCapactiy))
+                .where(checkMemberHasProfile(), checkIdGtThanCursor(cursor),
+                        checkMemberGenerationAndTeamAndPart(generation, team, part),
+                        checkMemberContainsName(name), checkMemberMbti(mbti), checkMemberSojuCapactiy(sojuCapactiy))
                 .groupBy(member.id)
                 .orderBy(getOrderByDropDownNumber(orderByDropDown))
                 .fetch();
