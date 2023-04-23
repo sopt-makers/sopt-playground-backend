a7da4be8943db6471415a5f03826cf30444af85d
yujindonut <bbb3631@naver.com>
2023-04-24 00:24:56 +0900

    feat: add team in controller

--- src/main/java/org/sopt/makers/internal/controller/MemberController.java
+++ src/main/java/org/sopt/makers/internal/controller/MemberController.java
@@ -209,7 +209,9 @@ public class MemberController {
                         0 -> 못마셔요 / 0.5 -> 0.5병 / 1.0 -> 1병 / 1.5 -> 1.5병 /
                         2.0 -> 2병 / 2.5 -> 2.5병 / 3.0 -> 3병 이상 \n
                     orderByDropDown : 
-                        1 -> 최근에 등록했순 / 2 -> 예전에 등록했순 / 3 -> 최근에 활동했순 / 4 -> 예전에 활동했순
+                        1 -> 최근에 등록했순 / 2 -> 예전에 등록했순 / 3 -> 최근에 활동했순 / 4 -> 예전에 활동했순 \n
+                    team : 
+                        임원진, 운영팀, 미디어팀, 메이커스
                     """
     )
     @GetMapping("/profile")
@@ -221,11 +223,12 @@ public class MemberController {
             @RequestParam(required = false, name = "generation") Integer generation,
             @RequestParam(required = false, name = "sojuCapactiy") Double sojuCapactiy,
             @RequestParam(required = false, name = "orderByDropDown") Integer orderByDropDown,
-            @RequestParam(required = false, name = "mbti") String mbti
+            @RequestParam(required = false, name = "mbti") String mbti,
+            @RequestParam(required = false, name = "team") String team
     ) {
         val members = limit == null ?
-                memberService.getMemberProfiles(filter, limit, cursor, name, generation, sojuCapactiy, orderByDropDown, mbti)
-                : memberService.getMemberProfiles(filter, limit + 1, cursor, name, generation, sojuCapactiy, orderByDropDown, mbti);
+                memberService.getMemberProfiles(filter, limit, cursor, name, generation, sojuCapactiy, orderByDropDown, mbti, team)
+                : memberService.getMemberProfiles(filter, limit + 1, cursor, name, generation, sojuCapactiy, orderByDropDown, mbti, team);
         val memberList = members.stream().map(memberMapper::toProfileResponse).collect(Collectors.toList());
         val hasNextMember = (limit != null && memberList.size() > limit);
         if (hasNextMember) memberList.remove(members.size() - 1);
