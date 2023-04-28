466d97009e84afbb783ed5e3664311e3c43e6c2d
JungWoo Song <37579681+bluayer@users.noreply.github.com>
2023-04-28 15:41:44 +0900

    fix: 적합한 이름으로 변경

--- src/main/java/org/sopt/makers/internal/dto/internal/InternalAllOfficialMemberResponse.java
+++ src/main/java/org/sopt/makers/internal/dto/internal/InternalAllOfficialMemberResponse.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public record InternalAllOfficialMemberResponse(
         List<InternalOfficialMemberResponse> members,
-        Integer numberOfMembers
+        int numberOfMembersAtGeneration
 ) {
 }
