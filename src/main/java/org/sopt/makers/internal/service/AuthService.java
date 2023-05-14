4a6ec5c20b61e1220e4e014036d7eeb227aea57d
yujindonut <bbb3631@naver.com>
2023-05-14 15:51:35 +0900

    feat: method명 변경

--- src/main/java/org/sopt/makers/internal/service/AuthService.java
+++ src/main/java/org/sopt/makers/internal/service/AuthService.java
@@ -256,7 +256,7 @@ public class AuthService {
         val memberActivities = memberHistories.stream().map(soptMemberHistory ->
                MemberSoptActivity.builder()
                        .memberId(member.getId())
-                       .team(isInOperationTeam(soptMemberHistory.getPart()) ? "운영진" : null)
+                       .team(isInManagementTeam(soptMemberHistory.getPart()) ? "운영진" : null)
                        .part(soptMemberHistory.getPart())
                        .generation(soptMemberHistory.getGeneration())
                        .build()).toList();
@@ -298,7 +298,7 @@ public class AuthService {
         return authConfig.getMagicRegisterToken();
     }
 
-    private boolean isInOperationTeam(String part) {
+    private boolean isInManagementTeam(String part) {
         return (part.contains("장") || part.equals("총무"));
     }
 
