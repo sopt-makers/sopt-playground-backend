7ffd016bb18b35c1bbc074119fc6a5f615db940b
JungWoo Song <37579681+bluayer@users.noreply.github.com>
2023-05-21 23:29:03 +0900

    CoffeeChat Email History (#154)
    * feat: email history entity 생성
    
    * feat: repository 생성
    
    * feat: 이력 저장

---
+++ src/main/java/org/sopt/makers/internal/domain/EmailHistory.java
@@ -0,0 +1,36 @@
+package org.sopt.makers.internal.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table(name = "email_history")
+public class EmailHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sender_id")
+    private Long senderId;
+
+    @Column(name = "receiver_id")
+    private Long receiverId;
+
+    @Column(name = "sender_email")
+    private String senderEmail;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
--- src/main/java/org/sopt/makers/internal/service/CoffeeChatService.java
+++ src/main/java/org/sopt/makers/internal/service/CoffeeChatService.java
@@ -2,21 +2,28 @@ package org.sopt.makers.internal.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.sopt.makers.internal.domain.EmailHistory;
 import org.sopt.makers.internal.domain.EmailSender;
 import org.sopt.makers.internal.dto.member.CoffeeChatRequest;
 import org.sopt.makers.internal.exception.BusinessLogicException;
 import org.sopt.makers.internal.exception.NotFoundDBEntityException;
+import org.sopt.makers.internal.repository.EmailHistoryRepository;
 import org.sopt.makers.internal.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RequiredArgsConstructor
 @Service
 public class CoffeeChatService {
     private final EmailSender emailSender;
     private final MemberRepository memberRepository;
+    private final EmailHistoryRepository emailHistoryRepository;
+
+    private final ZoneId KST = ZoneId.of("Asia/Seoul");
 
     public void sendCoffeeChatRequest (CoffeeChatRequest request, Long senderId) {
         val receiver  = memberRepository.findById(request.receiverId())
@@ -39,6 +46,14 @@ public class CoffeeChatService {
                     subject,
                     html
             );
+
+            emailHistoryRepository.save(EmailHistory.builder()
+                            .senderId(senderId)
+                            .receiverId(request.receiverId())
+                            .senderEmail(request.senderEmail())
+                            .category(request.category())
+                            .content(request.content())
+                            .createdAt(LocalDateTime.now(KST)).build());
         } catch (MessagingException | UnsupportedEncodingException exception) {
             exception.printStackTrace();
             throw new BusinessLogicException("커피챗 이메일 전송에 실패했습니다.");
