15df5172623c017a94c6c6dd8de4d48ba1013d49
JungWoo Song <37579681+bluayer@users.noreply.github.com>
2023-05-24 12:15:39 +0900

    feat: create sopticle service

---
+++ src/main/java/org/sopt/makers/internal/service/SopticleService.java
@@ -0,0 +1,34 @@
+package org.sopt.makers.internal.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.sopt.makers.internal.domain.Sopticle;
+import org.sopt.makers.internal.domain.SopticleWriter;
+import org.sopt.makers.internal.dto.SopticleSaveRequest;
+import org.sopt.makers.internal.repository.SopticleRepository;
+import org.sopt.makers.internal.repository.SopticleWriterRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class SopticleService {
+    private final SopticleRepository sopticleRepository;
+    private final SopticleWriterRepository sopticleWriterRepository;
+
+    @Transactional
+    public Sopticle createSopticle (SopticleSaveRequest request, Long userId) {
+        val sopticle = sopticleRepository.save(Sopticle.builder()
+                .link(request.link())
+                .userId(userId)
+                .build());
+        val sopticleWriters = request.writerIds().stream().map(id ->
+                SopticleWriter.builder()
+                        .writerId(id)
+                        .sopticleId(sopticle.getId())
+                        .build()).toList();
+        sopticleWriterRepository.saveAll(sopticleWriters);
+        return sopticle;
+    }
+}
