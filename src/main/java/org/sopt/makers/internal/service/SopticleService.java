7906ce5559c3fb9ea2933d9fe1f9a04c6d2dcb75
JungWoo Song <37579681+bluayer@users.noreply.github.com>
2023-05-27 00:23:54 +0900

    feat: validation added

--- src/main/java/org/sopt/makers/internal/service/SopticleService.java
+++ src/main/java/org/sopt/makers/internal/service/SopticleService.java
@@ -5,27 +5,34 @@ import lombok.val;
 import org.sopt.makers.internal.domain.Sopticle;
 import org.sopt.makers.internal.domain.SopticleWriter;
 import org.sopt.makers.internal.dto.SopticleSaveRequest;
+import org.sopt.makers.internal.exception.ClientBadRequestException;
+import org.sopt.makers.internal.repository.MemberRepository;
 import org.sopt.makers.internal.repository.SopticleRepository;
 import org.sopt.makers.internal.repository.SopticleWriterRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
 public class SopticleService {
     private final SopticleRepository sopticleRepository;
     private final SopticleWriterRepository sopticleWriterRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional
     public Sopticle createSopticle (SopticleSaveRequest request, Long userId) {
+        val writers = Set.copyOf(request.writerIds());
+        val isValidNumberOfWriters = memberRepository.countByIdIn(writers) == writers.size();
+        if (!isValidNumberOfWriters) throw new ClientBadRequestException("Wrong # of writers");
         val sopticle = sopticleRepository.save(Sopticle.builder()
                 .link(request.link())
                 .userId(userId)
                 .registeredAt(LocalDateTime.now())
                 .build());
-        val sopticleWriters = request.writerIds().stream().map(id ->
+        val sopticleWriters = writers.stream().map(id ->
                 SopticleWriter.builder()
                         .writerId(id)
                         .sopticleId(sopticle.getId())
