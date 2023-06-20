75b6b8026986826846a5847125854872a435aa98
yujindonut <bbb3631@naver.com>
2023-06-20 15:48:12 +0900

    chore: repository 메소드 수정

--- src/main/java/org/sopt/makers/internal/repository/WordChainGameRepository.java
+++ src/main/java/org/sopt/makers/internal/repository/WordChainGameRepository.java
@@ -3,9 +3,5 @@ package org.sopt.makers.internal.repository;
 import org.sopt.makers.internal.domain.WordChainGameRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface WordChainGameRepository extends JpaRepository<WordChainGameRoom, Long> {
-
-    Optional<WordChainGameRoom> findByIdOrderByCreatedAtDesc(Long roomId);
 }
--- src/main/java/org/sopt/makers/internal/repository/WordRepository.java
+++ src/main/java/org/sopt/makers/internal/repository/WordRepository.java
@@ -3,5 +3,9 @@ package org.sopt.makers.internal.repository;
 import org.sopt.makers.internal.domain.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WordRepository extends JpaRepository<Word, Long> {
+    List<Word> findByWordAndRoomId(String word, Long roomId);
+    Word findFirstByRoomIdOrderByCreatedAt(Long roomId);
 }
