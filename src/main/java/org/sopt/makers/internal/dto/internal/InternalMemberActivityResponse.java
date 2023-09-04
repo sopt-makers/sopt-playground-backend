1113deb18724c2a1f0a8eacec81872a2dd319bf9
yujindonut <bbb3631@naver.com>
2023-09-04 18:17:31 +0900

    add: appteam response 추가

---
+++ src/main/java/org/sopt/makers/internal/dto/internal/InternalMemberActivityResponse.java
@@ -0,0 +1,21 @@
+package org.sopt.makers.internal.dto.internal;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+
+public record InternalMemberActivityResponse(
+        @Schema(required = true)
+        Long id,
+        @Schema(required = true)
+        String name,
+        String profileImage,
+        @Schema(required = true)
+        List<InternalMemberActivityResponse.MemberSoptActivityResponse> activities
+) {
+    public record MemberSoptActivityResponse(
+            Long id,
+            Integer generation,
+            String part,
+            String team
+    ) {}
+}
