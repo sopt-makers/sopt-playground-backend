81a20ee678d7ce624c30b40061c9eac52227a5df
한유진 <78431728+yujindonut@users.noreply.github.com>
2023-10-01 23:57:07 +0900

    [CHORE] non null column (#232)
    * chore: nullable 하게 수정
    
    * chore: default 값 true로 변경

--- src/main/java/org/sopt/makers/internal/domain/Member.java
+++ src/main/java/org/sopt/makers/internal/domain/Member.java
@@ -121,9 +121,10 @@ public class Member {
     @Column(name = "has_profile")
     private Boolean hasProfile = true;
 
-    @Column(name = "edit_activities_able", nullable = false)
+    @Builder.Default
+    @Column(name = "edit_activities_able")
     @ColumnDefault("true")
-    private Boolean editActivitiesAble;
+    private Boolean editActivitiesAble = true;
 
     @Builder.Default
     @Column(name = "openToSoulmate")
