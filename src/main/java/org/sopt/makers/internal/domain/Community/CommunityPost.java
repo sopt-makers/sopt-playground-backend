65d79202b74307c5c4627676f6812c5b8ebdc225
yujindonut <bbb3631@naver.com>
2023-10-25 01:25:42 +0900

    chore: 클래스 명 변경

--- src/main/java/org/sopt/makers/internal/domain/Community/CommunityPost.java
+++ src/main/java/org/sopt/makers/internal/domain/Community/CommunityPost.java
@@ -14,7 +14,7 @@ import java.util.List;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Table
-public class Post {
+public class CommunityPost {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
