6fdcc3a47ed15dfa50cf88a1768419dcc5e3131a
yujindonut <bbb3631@naver.com>
2023-10-25 01:05:49 +0900

    chore: ci error 수정

--- src/test/java/org/sopt/makers/internal/InternalApplicationTests.java
+++ src/test/java/org/sopt/makers/internal/InternalApplicationTests.java
@@ -2,8 +2,10 @@ package org.sopt.makers.internal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
+@EnableJpaAuditing
 class InternalApplicationTests {
 
 	@Test
