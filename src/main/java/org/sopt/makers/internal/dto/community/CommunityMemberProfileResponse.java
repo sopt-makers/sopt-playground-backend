8b5020f318a27824ff72bcfc4978db05c583eaae
yujindonut <bbb3631@naver.com>
2023-10-30 01:28:27 +0900

    feat: unversion file

---
+++ src/main/java/org/sopt/makers/internal/dto/community/CommunityMemberProfileResponse.java
@@ -0,0 +1,51 @@
+package org.sopt.makers.internal.dto.community;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record CommunityMemberProfileResponse(
+        @Schema(required = true)
+        Long id,
+        @Schema(required = true)
+        String name,
+        String profileImage,
+        @Schema(required = true)
+        List<MemberSoptActivityResponse> activities,
+        List<MemberLinkResponse> links,
+        List<MemberCareerResponse> careers,
+        Boolean allowOfficial
+) {
+
+    public record UserFavorResponse(
+            Boolean isPourSauceLover,
+            Boolean isHardPeachLover,
+            Boolean isMintChocoLover,
+            Boolean isRedBeanFishBreadLover,
+            Boolean isSojuLover,
+            Boolean isRiceTteokLover
+    ){}
+
+    public record MemberLinkResponse(
+            Long id,
+            String title,
+            String url
+    ){}
+
+    public record MemberSoptActivityResponse(
+            Long id,
+            Integer generation,
+            String part,
+            String team
+    ){}
+
+    public record MemberCareerResponse(
+            Long id,
+            String companyName,
+            String title,
+            String startDate,
+            String endDate,
+            Boolean isCurrent
+    ){}
+}
