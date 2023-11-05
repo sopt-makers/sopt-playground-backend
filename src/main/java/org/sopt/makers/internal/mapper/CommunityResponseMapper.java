08390d16eaf4374fbdb03e278e7ad6a6f2039fbc
yujindonut <bbb3631@naver.com>
2023-11-05 19:05:10 +0900

    chore: mapper 수정

--- src/main/java/org/sopt/makers/internal/mapper/CommunityResponseMapper.java
+++ src/main/java/org/sopt/makers/internal/mapper/CommunityResponseMapper.java
@@ -1,6 +1,8 @@
 package org.sopt.makers.internal.mapper;
 
 import lombok.val;
+import org.sopt.makers.internal.domain.Member;
+import org.sopt.makers.internal.domain.MemberCareer;
 import org.sopt.makers.internal.dto.community.*;
 import org.springframework.stereotype.Component;
 
@@ -9,20 +11,30 @@ import java.util.stream.Collectors;
 
 @Component
 public class CommunityResponseMapper {
-
     public CommentResponse toCommentResponse(CommentDao dao) {
-        return new CommentResponse(dao.id(), dao.userId(), dao.userName(), dao.profileImage(), dao.activities(),
-                dao.careers(), dao.content(), dao.isBlindWriter(), dao.createdAt(), dao.updatedAt());
+        val member = toMemberResponse(dao.member());
+        val comment = dao.comment();
+        return new CommentResponse(comment.getId(), member, comment.getPostId(), comment.getParentCommentId(),
+                comment.getContent(), comment.getIsBlindWriter(), comment.getIsReported(), comment.getCreatedAt());
     }
 
-    public CommunityMemberResponse toMemberResponse(CategoryPostMemberDao dao) {
-        return new CommunityMemberResponse(dao.userId(), dao.userName(), dao.profileImage(),dao.activities(), dao.careers());
+    public CommunityPostMemberVo toPostVO(CategoryPostMemberDao dao) {
+        val member = toMemberResponse(dao.member());
+        return new CommunityPostMemberVo(member, dao.posts());
     }
 
-    public PostResponse toPostResponse (CategoryPostMemberDao dao, List<CommentDao> commentDaos) {
-        val member = toMemberResponse(dao);
+    public MemberVo toMemberResponse(Member member) {
+        val career = member.getCareers().stream().filter(MemberCareer::getIsCurrent).toList();
+        member.getActivities().sort((act1, act2) -> (act2.getGeneration() - act1.getGeneration()));
+        return new MemberVo(member.getId(),member.getName(), member.getProfileImage(),
+                member.getActivities().get(0), career.isEmpty() ? null : member.getCareers().get(0));
+    }
+
+    public PostResponse toPostResponse (CommunityPostMemberVo dao, List<CommentDao> commentDaos) {
+        val post = dao.posts();
+        val member = dao.member();
         val comments = commentDaos.stream().map(this::toCommentResponse).collect(Collectors.toList());
-        return new PostResponse(dao.id(), member, dao.userId(), dao.title(), dao.content(), dao.hits(), dao.images(),
-                dao.isQuestion(), dao.isBlindWriter(), dao.createdAt(), comments);
+        return new PostResponse(post.getId(),member, post.getWriterId(), post.getTitle(), post.getContent(),
+                post.getHits(), post.getImages(), post.getIsQuestion(), post.getIsBlindWriter(), post.getCreatedAt(), comments);
     }
 }
