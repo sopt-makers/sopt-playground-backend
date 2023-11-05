c9d9440d5f9bbb8879f197e94d135ce8fb14a9fe
yujindonut <bbb3631@naver.com>
2023-11-05 18:53:49 +0900

    add: community post member vo 생성

---
+++ src/main/java/org/sopt/makers/internal/dto/community/CommunityPostMemberVo.java
@@ -0,0 +1,12 @@
+package org.sopt.makers.internal.dto.community;
+
+import com.querydsl.core.annotations.QueryProjection;
+import org.sopt.makers.internal.domain.community.CommunityPost;
+
+public record CommunityPostMemberVo(
+        MemberVo member,
+        CommunityPost posts
+) {
+    @QueryProjection
+    public CommunityPostMemberVo {}
+}
