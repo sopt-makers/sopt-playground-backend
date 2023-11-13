196f72fa1c1d3b3a52bec6dd39e3c1b51f048486
yujindonut <bbb3631@naver.com>
2023-11-13 20:42:20 +0900

    chore: 필요없는 의존성 제거

--- src/main/java/org/sopt/makers/internal/repository/community/CommunityQueryRepository.java
+++ src/main/java/org/sopt/makers/internal/repository/community/CommunityQueryRepository.java
@@ -1,7 +1,6 @@
 package org.sopt.makers.internal.repository.community;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
