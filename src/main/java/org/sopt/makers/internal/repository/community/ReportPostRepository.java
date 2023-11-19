37ca8e6b254681614ccdd63fb56207ea706545f4
yujindonut <bbb3631@naver.com>
2023-11-20 01:40:20 +0900

    feat: report repository 생성

---
+++ src/main/java/org/sopt/makers/internal/repository/community/ReportPostRepository.java
@@ -0,0 +1,7 @@
+package org.sopt.makers.internal.repository.community;
+
+import org.sopt.makers.internal.domain.community.ReportPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportPostRepository extends JpaRepository<ReportPost, Long> {
+}
