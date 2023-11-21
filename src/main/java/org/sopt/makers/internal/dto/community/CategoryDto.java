7bb14ecccc385b8197093c384333969093b0c58b
yujindonut <bbb3631@naver.com>
2023-11-22 07:29:35 +0900

    chore: column명 변경

--- src/main/java/org/sopt/makers/internal/dto/community/CategoryDto.java
+++ src/main/java/org/sopt/makers/internal/dto/community/CategoryDto.java
@@ -1,14 +1,12 @@
 package org.sopt.makers.internal.dto.community;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.sopt.makers.internal.domain.community.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,7 +17,7 @@ public class CategoryDto {
     private String name;
     private String content;
     private Boolean hasAll;
-    private Boolean hasAnonymous;
+    private Boolean hasBlind;
     private Boolean hasQuestion;
     private List<CategoryDto> children;
 
@@ -27,7 +25,7 @@ public class CategoryDto {
         CategoryHelper helper = CategoryHelper.newInstance(
                 categories,
                 c -> new CategoryDto(c.getId(), c.getName(), c.getContent(), c.getHasAll(),
-                        c.getHasAnonymous(), c.getHasQuestion(), new ArrayList<>()),
+                        c.getHasBlind(), c.getHasQuestion(), new ArrayList<>()),
                 Category::getParent,
                 Category::getId,
                 CategoryDto::getChildren);
