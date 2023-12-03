e7dbb6b999639c53f4d44080a6c9e87e913e29b7
yujindonut <bbb3631@naver.com>
2023-12-04 00:19:54 +0900

    feat: community mapper객체 생성

---
+++ src/main/java/org/sopt/makers/internal/mapper/CommunityMapper.java
@@ -0,0 +1,13 @@
+package org.sopt.makers.internal.mapper;
+
+import org.mapstruct.Mapper;
+import org.sopt.makers.internal.domain.community.CommunityComment;
+import org.sopt.makers.internal.domain.community.CommunityPost;
+import org.sopt.makers.internal.domain.community.DeletedCommunityComment;
+import org.sopt.makers.internal.domain.community.DeletedCommunityPost;
+
+@Mapper(componentModel = "spring")
+public interface CommunityMapper {
+    DeletedCommunityComment toDeleteCommunityComment (CommunityComment comment);
+    DeletedCommunityPost toDeleteCommunityPost (CommunityPost post);
+}
