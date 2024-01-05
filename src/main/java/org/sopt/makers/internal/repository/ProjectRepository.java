2a52fa40de4ca81a1a6be483c8694fb0cbf1a89c
yujindonut <bbb3631@naver.com>
2024-01-06 04:24:41 +0900

    feat: find project by name

--- src/main/java/org/sopt/makers/internal/repository/ProjectRepository.java
+++ src/main/java/org/sopt/makers/internal/repository/ProjectRepository.java
@@ -3,5 +3,8 @@ package org.sopt.makers.internal.repository;
 import org.sopt.makers.internal.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findAllByNameContaining(String name);
 }
