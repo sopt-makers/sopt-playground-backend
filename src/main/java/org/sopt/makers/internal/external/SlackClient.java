d7ce3b892b333d5e00fda88d934ff66564319655
yujindonut <bbb3631@naver.com>
2024-01-17 18:00:51 +0900

    feat: slack 채널알람 url 분기

--- src/main/java/org/sopt/makers/internal/external/SlackClient.java
+++ src/main/java/org/sopt/makers/internal/external/SlackClient.java
@@ -5,9 +5,11 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.Map;
 @FeignClient(value = "slackClient", url = "${slack.url}")
 public interface SlackClient {
-    @PostMapping(value = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    void postMessage(@RequestBody String request);
+    @PostMapping(value = "${slack.newbie-channel}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    void postNewProfileMessage(@RequestBody String request);
+
+    @PostMapping(value = "${slack.community-report-channel}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    void postReportMessage(@RequestBody String request);
 }
