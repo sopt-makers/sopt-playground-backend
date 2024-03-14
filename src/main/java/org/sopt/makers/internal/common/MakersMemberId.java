65b99937e862b7ce6fb90e9c878318eeb02b8f3f
Lee SeungHeon <51286325+dev-Crayon@users.noreply.github.com>
2024-03-15 00:00:59 +0900

    4기 메이커스 멤버 White list (#334)
    Related to #332

--- src/main/java/org/sopt/makers/internal/common/MakersMemberId.java
+++ src/main/java/org/sopt/makers/internal/common/MakersMemberId.java
@@ -1,21 +1,16 @@
 package org.sopt.makers.internal.common;
 
 import lombok.Getter;
-import lombok.val;
 
 import java.util.List;
 
-
 public class MakersMemberId {
+    @Getter
     private static final List<Long> makersMember = List.of(
-            1L, 2L, 258L, 3L, 259L, 4L, 260L, 5L, 6L, 7L, 8L, 264L, 9L, 265L, 10L, 13L, 21L, 22L, 23L, 281L,
-            26L, 282L, 283L, 28L, 29L, 285L, 30L, 286L, 31L, 32L, 33L, 34L, 35L, 36L, 37L, 38L, 294L, 39L, 40L, 554L,
-            43L, 44L, 45L, 46L, 303L, 559L, 51L, 563L, 569L, 58L, 570L, 59L, 60L, 61L, 64L, 66L, 72L, 78L, 85L,
-            99L, 357L, 358L, 107L, 112L, 370L, 115L, 128L, 390L, 396L, 143L, 144L, 401L, 410L, 171L, 173L, 180L,
-            186L, 187L, 188L, 192L, 203L, 204L, 205L, 227L, 238L, 251L
+            4L, 10L, 15L, 30L, 35L, 47L, 61L, 62L, 64L, 66L,
+            72L, 78L, 85L, 115L, 123L, 143L, 171L, 175L, 180L, 192L,
+            203L, 204L, 251L, 260L, 306L, 319L, 325L, 333L, 341L, 346L,
+            357L, 370L, 388L, 390L, 465L, 561L, 569L, 585L, 635L, 646L,
+            647L, 688L
     );
-
-    public static List<Long> getMakersMember() {
-        return makersMember;
-    }
 }
