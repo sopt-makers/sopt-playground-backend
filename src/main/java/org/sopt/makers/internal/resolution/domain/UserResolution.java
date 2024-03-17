b7ad6841bd3dad43de963683bff57ecc98d480c2
Lee SeungHeon <51286325+dev-Crayon@users.noreply.github.com>
2024-03-17 15:46:04 +0900

    [Feat]: 유저 다짐 메시지 저장 Entity Class 구현 (#338)
    UserResolution.java 파일 생성
    
    Related to: #337

---
+++ src/main/java/org/sopt/makers/internal/resolution/domain/UserResolution.java
@@ -0,0 +1,29 @@
+package org.sopt.makers.internal.resolution.domain;
+
+import lombok.*;
+import org.sopt.makers.internal.domain.Member;
+import org.sopt.makers.internal.domain.common.AuditingTimeEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class UserResolution extends AuditingTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column
+    private String tagIds;
+}
