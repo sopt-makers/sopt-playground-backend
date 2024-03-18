d2671faa833c4063bcd4a9b9c5a83803a75e0c30
Yejun Park <80024278+jun02160@users.noreply.github.com>
2024-03-18 12:35:07 +0900

    [Feat] 다짐 메시지 생성 API 개발 (#340)

---
+++ src/main/java/org/sopt/makers/internal/resolution/controller/UserResolutionController.java
@@ -0,0 +1,40 @@
+package org.sopt.makers.internal.resolution.controller;
+
+import java.util.Map;
+
+import org.sopt.makers.internal.domain.InternalMemberDetails;
+import org.sopt.makers.internal.resolution.dto.request.ResolutionSaveRequest;
+import org.sopt.makers.internal.resolution.service.UserResolutionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/resolution")
+@SecurityRequirement(name = "Authorization")
+@Tag(name = "다짐 메시지 관련 API", description = "다짐 메시지 관련 API List")
+public class UserResolutionController {
+
+	private final UserResolutionService userResolutionService;
+
+	@Operation(summary = "다짐 메시지 생성")
+	@PostMapping
+	public ResponseEntity<Map<String, Boolean>> createResolution(
+		@Parameter(hidden = true) @AuthenticationPrincipal InternalMemberDetails memberDetails,
+		@RequestBody ResolutionSaveRequest request
+	) {
+		userResolutionService.createResolution(memberDetails.getId(), request);
+		return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("다짐 메시지 생성 성공", true));
+	}
+}
---
+++ src/main/java/org/sopt/makers/internal/resolution/domain/ResolutionTag.java
@@ -0,0 +1,42 @@
+package org.sopt.makers.internal.resolution.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.sopt.makers.internal.exception.ClientBadRequestException;
+
+public enum ResolutionTag {
+
+	SKILL_ENHANCEMENT("능력 향상", 1),
+	IT_KNOWLEDGE("IT 지식", 2),
+	FRIENDSHIP("친목", 3),
+	ENTREPRENEURSHIP_FOUNDATION("창업 기반", 4),
+	COLLABORATION_EXPERIENCE("협업 경험", 5),
+	GREAT_TEAM("좋은 팀", 6);
+
+	private final String value;
+	private final int index;
+
+	private static final Map<String, ResolutionTag> TAG_MAP = Arrays.stream(ResolutionTag.values())
+		.collect(Collectors.toMap(tag -> tag.value, tag -> tag));
+
+	ResolutionTag(String value, int index) {
+		this.value = value;
+		this.index = index;
+	}
+
+	public static String getTagIds(List<String> tagNames) {
+		return tagNames.stream()
+			.map(tag -> String.valueOf(TAG_MAP.get(validate(tag)).index))
+			.collect(Collectors.joining(","));
+	}
+
+	public static String validate(String value) {
+		if (TAG_MAP.get(value) == null) {
+			throw new ClientBadRequestException("Unknown Tag Name");
+		}
+		return value;
+	}
+}
