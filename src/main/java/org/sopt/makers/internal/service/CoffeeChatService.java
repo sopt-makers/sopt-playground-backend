d54df6e961b84ca1cf8ca5253303f1084b8d322b
Yejun Park <80024278+jun02160@users.noreply.github.com>
2024-08-20 22:41:38 +0900

    chore: random sort 추가 (#439)

--- src/main/java/org/sopt/makers/internal/service/CoffeeChatService.java
+++ src/main/java/org/sopt/makers/internal/service/CoffeeChatService.java
@@ -20,6 +20,7 @@ import javax.mail.MessagingException;
 import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Collections;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -68,7 +69,9 @@ public class CoffeeChatService {
     }
 
     public List<CoffeeChatVo> getCoffeeChatList () {
-        val members = memberRepository.findAllByIsCoffeeChatActivateTrueOrderByCoffeeChatUpdatedAtDesc();
+        val members = memberRepository.findAllByIsCoffeeChatActivateTrue();
+        Collections.shuffle(members);
+
         return members.stream().map(
             m -> {
                 val career = getCurrentMemberCareer(m);
