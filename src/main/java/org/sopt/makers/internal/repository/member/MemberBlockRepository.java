7cfe36de0c2ccfe5e4fd1d7a6f2e807dabc56af2
dev-Crayon <seungheon328@gmail.com>
2024-09-09 17:36:34 +0900

    [Feat]: 유저 차단 및 신고 repository 구현
    Related to: #455

---
+++ src/main/java/org/sopt/makers/internal/repository/member/MemberBlockRepository.java
@@ -0,0 +1,7 @@
+package org.sopt.makers.internal.repository.member;
+
+import org.sopt.makers.internal.domain.member.MemberBlock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberBlockRepository extends JpaRepository<MemberBlock, Long> {
+}
