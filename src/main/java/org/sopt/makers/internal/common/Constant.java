5f358f20b81affa21049ec6178569844f97877b5
jun02160 <yejun@ab180.co>
2024-09-14 21:52:51 +0900

    chore: 최신 기수 업데이트

--- src/main/java/org/sopt/makers/internal/common/Constant.java
+++ src/main/java/org/sopt/makers/internal/common/Constant.java
@@ -2,7 +2,7 @@ package org.sopt.makers.internal.common;
 
 public class Constant {
 
-	public final static int CURRENT_GENERATION = 34;
+	public final static int CURRENT_GENERATION = 35;
 
 	// Regex
 	public final static String PHONE_NUMBER_REGEX = "^010\\d{8}$";
