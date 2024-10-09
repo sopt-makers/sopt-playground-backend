3a7037bd863f115110eb7675aef48b5d237ca237
dev-Crayon <seungheon328@gmail.com>
2024-10-09 18:09:39 +0900

    [Refactor]: CoffeeChat Entity class 정의
    Member entity class와 일대일 연관관계를 맺은 CoffeeChat entity class 구현
    createdAt, updatedAt 필드의 nullable = false 설정 추가
    
    Related to: #480

---
+++ src/main/java/org/sopt/makers/internal/member/domain/CoffeeChat.java
@@ -0,0 +1,37 @@
+package org.sopt.makers.internal.member.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+import org.hibernate.annotations.ColumnDefault;
+import org.sopt.makers.internal.domain.Member;
+import org.sopt.makers.internal.domain.common.AuditingTimeEntity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CoffeeChat extends AuditingTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Builder.Default
+    @Column(nullable = false)
+    @ColumnDefault("true")
+    private Boolean isCoffeeChatActivate = true;
+
+    @Column(nullable = false, length = 40)
+    private String coffeeChatBio;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+}
