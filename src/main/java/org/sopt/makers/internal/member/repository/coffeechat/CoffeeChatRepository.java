546abf8475523de8ac44aefe63584ed4b5ee4d10
dev-Crayon <seungheon328@gmail.com>
2024-10-09 18:16:43 +0900

    [Refactor]: CoffeeChatRepository 인터페이스 생성
    CoffeeChat Entity에 접근하기 위한 repository 파일을 생성했습니다.
    
    Related to: #480

--- src/main/java/org/sopt/makers/internal/member/repository/coffeechat/CoffeeChatRepository.java
+++ src/main/java/org/sopt/makers/internal/member/repository/coffeechat/CoffeeChatRepository.java
@@ -1,4 +1,4 @@
-package org.sopt.makers.internal.member.repository;
+package org.sopt.makers.internal.member.repository.coffeechat;
 
 import org.sopt.makers.internal.member.domain.coffeechat.CoffeeChat;
 import org.springframework.data.jpa.repository.JpaRepository;
