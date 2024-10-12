38820f1251e7fd2a6644817e3f6608533090e455
jun02160 <yejun@ab180.co>
2024-10-12 22:04:14 +0900

    chore: 코드리뷰 반영
    - RequestBody에서 ChatCategory 타입을 사용하도록 변경
    - ChatCategory에 정의된 값들 중에 존재하는지 검사하는 validation 추가
    
    related issue #483

--- src/main/java/org/sopt/makers/internal/dto/member/CoffeeChatRequest.java
+++ src/main/java/org/sopt/makers/internal/dto/member/CoffeeChatRequest.java
@@ -1,6 +1,7 @@
 package org.sopt.makers.internal.dto.member;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.sopt.makers.internal.member.domain.coffeechat.ChatCategory;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -14,7 +15,7 @@ public record CoffeeChatRequest(
         @JsonProperty(required = false) String senderPhone,
 
 		@NotBlank(message = "수신 카테고리는 필수 입력 값입니다.")
-		String category,
+		ChatCategory category,
 
 		@NotBlank(message = "수신 본문은 필수 입력 값입니다.")
 		@Size(max = 40, message = "본문은 ??자를 초과할 수 없습니다.")  // TODO 정책 fix 되면 반영 예정
--- src/main/java/org/sopt/makers/internal/member/domain/coffeechat/ChatCategory.java
+++ src/main/java/org/sopt/makers/internal/member/domain/coffeechat/ChatCategory.java
@@ -1,6 +1,10 @@
 package org.sopt.makers.internal.member.domain.coffeechat;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
+import org.sopt.makers.internal.exception.ClientBadRequestException;
+
+import java.util.Arrays;
 
 @Getter
 public enum ChatCategory {
@@ -15,5 +19,13 @@ public enum ChatCategory {
 
 	ChatCategory(String value) {
 		this.value = value;
+	}
+
+	@JsonCreator
+	public static ChatCategory fromValue(String value) {
+		return Arrays.stream(ChatCategory.values())
+				.filter(category -> category.value.equals(value))
+				.findFirst()
+				.orElseThrow(() -> new ClientBadRequestException("Unknown Chat Category Value: " + value));
 	}
 }
