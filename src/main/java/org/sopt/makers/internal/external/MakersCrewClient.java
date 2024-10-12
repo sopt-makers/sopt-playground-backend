06d3d66b7f76983a8f8d62429a8086dd1f4ab4dd
jun02160 <yejun@ab180.co>
2024-10-12 17:28:23 +0900

    chore: update feign client value
    - makersCrewProdClient -> makersCrewClient로 변경
    
    related issue #479

--- src/main/java/org/sopt/makers/internal/external/MakersCrewClient.java
+++ src/main/java/org/sopt/makers/internal/external/MakersCrewClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "makersCrewProdClient", url = "${internal.crew.url}")
+@FeignClient(value = "makersCrewClient", url = "${internal.crew.url}")
 public interface MakersCrewClient {
 
     @GetMapping("/meeting/v2/org-user")
