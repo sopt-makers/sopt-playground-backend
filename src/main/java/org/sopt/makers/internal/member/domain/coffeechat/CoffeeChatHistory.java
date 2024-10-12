cb8741c16c93b16eb5b02caeacd12bb5efcc242e
jun02160 <yejun@ab180.co>
2024-10-12 19:34:28 +0900

    feat: 커피챗 이력 관리 테이블 추가
    - EmailHistory와 비슷한 역할을 수행하나, 추후 별점 등 커피챗 별도의 기능을 추가할 수 있는 확장성을 고려해 신규 테이블 생성
    
    related issue #483

---
+++ src/main/java/org/sopt/makers/internal/member/domain/coffeechat/CoffeeChatHistory.java
@@ -0,0 +1,40 @@
+package org.sopt.makers.internal.member.domain.coffeechat;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+import org.sopt.makers.internal.domain.Member;
+import org.sopt.makers.internal.domain.common.AuditingTimeEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CoffeeChatHistory extends AuditingTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "receiver_id", nullable = false)
+	private Member receiver;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "sender_id", nullable = false)
+	private Member sender;
+
+	private String requestContent;
+
+	@Builder
+	private CoffeeChatHistory(Member receiver, Member sender, String requestContent) {
+		this.receiver = receiver;
+		this.sender = sender;
+		this.requestContent = requestContent;
+	}
+}
