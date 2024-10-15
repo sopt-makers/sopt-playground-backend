c12bafd5fac94607ed6f231deb066ab68a4fb1e6
jun02160 <yejun@ab180.co>
2024-10-16 00:41:53 +0900

    chore: text 수정
    related issue #483

--- src/main/java/org/sopt/makers/internal/external/gabia/SmsChatSender.java
+++ src/main/java/org/sopt/makers/internal/external/gabia/SmsChatSender.java
@@ -16,12 +16,12 @@ public class SmsChatSender implements MessageSender {
 
 	@Override
 	public void sendMessage(Member sender, Member receiver, String content, String replyInfo, ChatCategory category) {
-		String message = "[Web발신][SOPT makers] 커피챗 신청이 도착했어요!\n" +
-				"전달드린 전화번호로 직접 연결어쩌고해저쩌고주세요.\n\n" +
-				"- 이름 : " + sender.getName() + "\n" +
-				"- 연락처 : " + replyInfo + "\n" +
-				"- 파트 : " + memberRetriever.concatPartAndGeneration(sender.getId()) + "\n" +
-				"- 멤버 프로필 링크 : https://playground.sopt.org/members/" + sender.getId() + "\n\n" +
+		String message = "[SOPT makers] 커피챗 제안이 도착했어요!\n" +
+				"연결을 원하신다면 멤버의 전화번호로 직접 연락해 주세요.\n\n" +
+				"- 이름: " + sender.getName() + "\n" +
+				"- 연락처: " + replyInfo + "\n" +
+				"- 파트: " + memberRetriever.concatPartAndGeneration(sender.getId()) + "\n" +
+				"- 멤버 프로필 링크: https://playground.sopt.org/members/" + sender.getId() + "\n\n" +
 				"- 이런 내용이 궁금해요\n" +
 				content;
 
