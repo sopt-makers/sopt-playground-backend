75dc8b5b9ea571949c0bfafa79f6989140d127e2
dev-Crayon <seungheon328@gmail.com>
2024-10-17 18:02:24 +0900

    [Feat]: 최근 커피챗 조회 API Controller 구현
    최근 커피챗 정보를 조회하기 위한 Controller 메서드 구현
    TODO -> coffeeChatService에서 getRecentCoffeeChatList() 메서드를 구현해야합니다.
    
    Related to: #492

--- src/main/java/org/sopt/makers/internal/member/controller/coffeechat/CoffeeChatController.java
+++ src/main/java/org/sopt/makers/internal/member/controller/coffeechat/CoffeeChatController.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.sopt.makers.internal.dto.member.CoffeeChatResponse;
 import org.sopt.makers.internal.dto.member.CoffeeChatResponse.CoffeeChatVo;
+import org.sopt.makers.internal.member.controller.coffeechat.dto.response.RecentCoffeeChatResponse;
+import org.sopt.makers.internal.member.controller.coffeechat.dto.response.RecentCoffeeChatResponse.RecentCoffeeChat;
 import org.sopt.makers.internal.member.service.coffeechat.CoffeeChatService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,14 @@ import java.util.List;
 public class CoffeeChatController {
 
     private final CoffeeChatService coffeeChatService;
+
+    @Operation(summary = "최근 진행된 커피챗 유저 조회 API")
+    @GetMapping("/recent")
+    public ResponseEntity<RecentCoffeeChatResponse> getRecentCoffeeChatList() {
+//        List<RecentCoffeeChat> recentCoffeeChatList = coffeeChatService.getRecentCoffeeChatList();
+//        return ResponseEntity.status(HttpStatus.OK).body(new RecentCoffeeChatResponse(recentCoffeeChatList));
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
 
     @Operation(summary = "커피챗 활성 유저 조회 API")
     @GetMapping("")
---
+++ src/main/java/org/sopt/makers/internal/member/controller/coffeechat/dto/response/RecentCoffeeChatResponse.java
@@ -0,0 +1,27 @@
+package org.sopt.makers.internal.member.controller.coffeechat.dto.response;
+
+import java.util.List;
+
+public record RecentCoffeeChatResponse(
+
+        List<RecentCoffeeChat> recentCoffeeChatList
+) {
+    public record RecentCoffeeChat(
+
+            Long memberId,
+
+            String bio,
+
+            List<String> sectionList,
+
+            String name,
+
+            String career,
+
+            String organization,
+
+            String companyJob,
+
+            List<String> soptActivities
+    ) {}
+}
