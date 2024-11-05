5d18652c6eec7063e51d587b6050e81c407cfaa8
jun02160 <yejun@ab180.co>
2024-11-05 23:26:11 +0900

    chore: 문자 발송 시 '[]' 문자 제거

--- src/main/java/org/sopt/makers/internal/member/service/MemberRetriever.java
+++ src/main/java/org/sopt/makers/internal/member/service/MemberRetriever.java
@@ -9,6 +9,7 @@ import org.sopt.makers.internal.member.repository.soptactivity.MemberSoptActivit
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
