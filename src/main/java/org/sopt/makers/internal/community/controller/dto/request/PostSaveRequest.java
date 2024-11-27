ce89e5a9dd71da9c1bf3f07647ef671a3a199b58
dev-Crayon <seungheon328@gmail.com>
2024-11-27 18:57:01 +0900

    [Refactor]: request body validation 추가
    Related to: #559

--- src/main/java/org/sopt/makers/internal/community/controller/dto/request/PostSaveRequest.java
+++ src/main/java/org/sopt/makers/internal/community/controller/dto/request/PostSaveRequest.java
@@ -2,20 +2,30 @@ package org.sopt.makers.internal.community.controller.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public record PostSaveRequest(
-    @Schema(required = true)
-    Long categoryId,
+        @Schema(required = true)
+        @NotNull
+        Long categoryId,
 
-    String title,
+        String title,
 
-    @Schema(required = true)
-    String content,
+        @Schema(required = true)
+        @NotBlank
+        String content,
 
-    @Schema(required = true)
-    Boolean isQuestion,
+        @Schema(required = true)
+        @NotNull
+        Boolean isQuestion,
 
-    @Schema(required = true)
-    Boolean isBlindWriter,
+        @Schema(required = true)
+        @NotNull
+        Boolean isBlindWriter,
 
-    String[] images
-) {}
+        @Schema(required = true)
+        @NotNull
+        String[] images
+) {
+}
