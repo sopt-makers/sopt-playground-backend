e56b2aa645535eb77a4d9d0bd85519b98e71b6be
dev-Crayon <seungheon328@gmail.com>
2024-11-27 15:37:39 +0900

    [Refactor]: AnonymousProfileImageRepository 패키지 변경
    Related to: #559

--- src/main/java/org/sopt/makers/internal/community/repository/anonymous/AnonymousProfileImageRepository.java
+++ src/main/java/org/sopt/makers/internal/community/repository/anonymous/AnonymousProfileImageRepository.java
@@ -1,4 +1,4 @@
-package org.sopt.makers.internal.community.repository;
+package org.sopt.makers.internal.community.repository.anonymous;
 
 import org.sopt.makers.internal.community.domain.AnonymousProfileImage;
 import org.springframework.data.jpa.repository.JpaRepository;
