ffec75066ee594e62efd1ce3cd3b919508f937d6
Yejun Park <80024278+jun02160@users.noreply.github.com>
2025-01-30 13:56:06 +0900

    [Feat] 마이 솝트 리포트 공통 데이터 조회 API  (#580)
    * feat: 마이 솝트 리포트 데이터 조회 API 추가
    
    - json data serializer를 통해 형 변환 및 응답을 데이터 그대로 내려주도록 구현
    
    related_issue: #577
    
    * feat: request param default value 추가
    
    related_issue: #577
    
    * chore: url 변경
    
    - /api/v1/stats -> /api/v1/report/stats
    
    related_issue: #577
    
    * feat: 마이솝트리포트 - 마이 플그 파트 API 추가
    
    - 쿼리로 조회가능한 데이터만 구현
    
    related_issue: #581
    
    * chore: group -> crew
    
    related_issue: #581
    
    * feat: AmplitudeClient 통신 추가
    
    - amplitude rest api의 응답 결과 데이터를 기반으로 반환
    - 조회 관련 데이터 integer -> long
    - year, startDate, endDate 공통 상수로 이동
    
    related_issue: #581
    
    * feat: local cache 적용
    
    related_issue: #581
    
    * chore: 불필요한 로깅 제거
    
    related_issue: #581
    
    * feat: pagePath 추가
    
    related_issue: #581
    
    * chore: totalDurationTime 제외
    
    related_issue: #581
    
    * chore: crew 임시 값 변경
    
    related_issue: #581
    
    * chore: SoptReportCategory enum으로 생성
    
    related_issue: #580
    
    * chore: 코드리뷰 반영
    
    - fetchOne() null 처리
    - @transactional 추가
    
    related_issue: #582
    
    * chore: default type 추가
    
    related_issue: #582
    
    * fix: unique 속성 추가
    
    related_issue: #580
    
    * docs: test application.yml update
    
    related_issue: #580

--- src/main/java/org/sopt/makers/internal/common/Constant.java
+++ src/main/java/org/sopt/makers/internal/common/Constant.java
@@ -1,9 +1,16 @@
 package org.sopt.makers.internal.common;
 
+import java.time.LocalDateTime;
+
 public class Constant {
 
 	public final static int CURRENT_GENERATION = 35;
 
+	public final static Integer REPORT_FILTER_YEAR = 2024;
+	public final static LocalDateTime START_DATE_OF_YEAR = LocalDateTime.of(REPORT_FILTER_YEAR, 1, 1, 0, 0);
+	public final static LocalDateTime END_DATE_OF_YEAR = LocalDateTime.of(REPORT_FILTER_YEAR, 12, 31, 23, 59);
+
 	// Regex
 	public final static String PHONE_NUMBER_REGEX = "^(010|015)\\d{8}$";
+
 }
