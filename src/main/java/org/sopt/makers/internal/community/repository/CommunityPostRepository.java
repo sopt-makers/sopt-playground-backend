c4d1146e4af5193e6c1fce8940b8c7ca31c6736c
Lee SeungHeon <51286325+dev-Crayon@users.noreply.github.com>
2025-03-21 20:25:56 +0900

    Refactor/#589 view count (#590)
    * [Feat]: 참여한 커피챗 타이틀 조회 API
    
    controller 메서드 및 response dto 정의
    
    Related to: #578
    
    * [Feat]: 참여한 커피챗 타이틀 조회 API
    
    retriever & service 계층 구현
    
    Related to: #578
    
    * [Feat]: 참여한 커피챗 타이틀 조회 API
    
    querydsl 코드 작성
    
    Related to: #578
    
    * [Feat]: 참여한 커피챗 타이틀 조회 API
    
    response 응답값 추가
    커피챗 오픈한 사람의 이름
    커리팻 오른한 사람의 커리어
    
    Related to: #578
    
    * [Feat]: 커피챗 리뷰 생성 API controller 및 request body, response body 구현
    
    Related to: #578
    
    * [Feat]: 커피챗 리뷰 생성 API 서비스 로직 구현
    
    참여한 커피챗인지 확인
    이미 등록한 리뷰가 있는지 확인
    리뷰 데이터 생성 로직 구현
    
    Related to: #578
    
    * [Feat]: 커피챗 리뷰 생성 관련 repository 파일 생성
    
    Related to: #578
    
    * [Feat]: 커피챗 리뷰 Entity 구현
    
    Related to: #578
    
    * [Feat]: 커피챗 리뷰 조회 controller 및 response dto 구현
    
    Related to: #578
    
    * [Feat]: 커피챗 리뷰 조회 service 계층 구현
    
    Related to: #578
    
    * [Feat]: 커리팻 리뷰 6개 불러오는 JPA 메서드 작성
    
    Related to: #578
    
    * [Feat]: 커피챗 리뷰 API 코드리뷰 반영
    
    endpoint restful api 경로로 변경
    response 필드명 테이블 컬럼명과 통일
    
    Related to: #578
    
    * [Refactor]: 조회수 충돌 3회 이상 발생시 conflict 에러 반환을 위한 클래스 구현
    
    ConflictException.java 생성 및 내부 구현
    
    Related to: #589
    
    * [Refactor]: 낙관적락 충돌감지를 위한 버전 컬럼 생성
    
    CommunityPost.java에 Version 컬럼 추가
    
    Related to: #589
    
    * [Refactor]: 커뮤니티 조회수 수동 업데이트 쿼리 적용
    
    사용되지 않는 낙관적 락 버전 필드 삭제
    인스턴스 업데이트 메서드 삭제
    수동 업데이트 쿼리 적용
    
    Related to: #589
    
    * [Refactor]: 사용되지 않는 ConflictException 파일 삭제
    
    Related to: #589

--- src/main/java/org/sopt/makers/internal/community/repository/CommunityPostRepository.java
+++ src/main/java/org/sopt/makers/internal/community/repository/CommunityPostRepository.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 
 import org.sopt.makers.internal.community.domain.CommunityPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommunityPostRepository extends JpaRepository<CommunityPost, Long>, CommunityPostRepositoryCustom {
     Optional<CommunityPost> findById(Long id);
@@ -13,4 +16,8 @@ public interface CommunityPostRepository extends JpaRepository<CommunityPost, Lo
     List<CommunityPost> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 
     Integer countAllByMemberIdAndCreatedAtBetween(Long memberId, LocalDateTime start, LocalDateTime end);
+
+    @Modifying
+    @Query("UPDATE CommunityPost p SET p.hits = p.hits + 1 WHERE p.id = :postId")
+    void increaseHitsDirect(@Param("postId") Long postId);
 }
