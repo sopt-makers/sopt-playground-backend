c071f9528feb04e68705e8467305fe7251cf238e
임주민 <limj1213@gmail.com>
2025-03-24 22:34:01 +0900

    [Feat]: 인덱스가 아닌 enum 영문값으로 저장되도록 구현

--- src/main/java/org/sopt/makers/internal/resolution/dto/response/ResolutionResponse.java
+++ src/main/java/org/sopt/makers/internal/resolution/dto/response/ResolutionResponse.java
@@ -1,12 +1,14 @@
 package org.sopt.makers.internal.resolution.dto.response;
 
+import org.sopt.makers.internal.resolution.domain.ResolutionTag;
+
 import java.util.List;
 
 public record ResolutionResponse(
 
         String memberImageUrl,
         String memberName,
-        List<String> tags,
+        List<ResolutionTag> tags,
         String content
 ) {
 }
