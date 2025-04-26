316634add3f7c9ab15ffa1328b83ea7e0d2550a4
임주민 <limj1213@gmail.com>
2025-04-26 22:55:29 +0900

    [Chore]: 필요없는 솝티클 관련 파일들 삭제

--- src/main/java/org/sopt/makers/internal/dto/sopticle/SopticleSaveRequest.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.makers.internal.dto.sopticle;
-
-import java.util.List;
-
-public record SopticleSaveRequest(
-        String link,
-        List<Long> writerIds
-) {
-}
--- src/main/java/org/sopt/makers/internal/repository/SopticleRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sopt.makers.internal.repository;
-
-import org.sopt.makers.internal.domain.Sopticle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SopticleRepository extends JpaRepository<Sopticle, Long> {
-}
