fa0dd0a83c7b5d4750e268de1b5f2375f146deaf
임주민 <limj1213@gmail.com>
2025-05-16 15:18:39 +0900

    [Chore]: memberVo 변환 올바른 정렬 로직으로 변경 및 mapper 선언 없이 dto안에 위치하도록 수정

--- src/main/java/org/sopt/makers/internal/community/dto/MemberVo.java
+++ src/main/java/org/sopt/makers/internal/community/dto/MemberVo.java
@@ -1,7 +1,11 @@
 package org.sopt.makers.internal.community.dto;
 
+import lombok.val;
+import org.sopt.makers.internal.member.domain.Member;
 import org.sopt.makers.internal.member.domain.MemberCareer;
 import org.sopt.makers.internal.member.domain.MemberSoptActivity;
+
+import java.util.Comparator;
 
 public record MemberVo(
         Long id,
@@ -9,4 +13,20 @@ public record MemberVo(
         String profileImage,
         MemberSoptActivity activity,
         MemberCareer careers
-) {}
+) {
+    public static MemberVo of(Member member) {
+        MemberCareer career = (member.getCareers() == null || member.getCareers().stream().noneMatch(MemberCareer::getIsCurrent)) ?
+                null : member.getCareers().stream().filter(MemberCareer::getIsCurrent).toList().get(0);
+        MemberSoptActivity activity = member.getActivities().stream()
+                .max(Comparator.comparingInt(MemberSoptActivity::getGeneration))
+                .orElse(null);
+
+        return new MemberVo(
+                member.getId(),
+                member.getName(),
+                member.getProfileImage(),
+                activity,
+                career
+        );
+    }
+}
