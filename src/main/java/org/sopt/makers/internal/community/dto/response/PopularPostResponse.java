a61dba62dadfdadcd34d84a891abaf9ba77c1b7a
임주민 <limj1213@gmail.com>
2025-05-20 15:27:25 +0900

    [Feat]: member에 대해 id, name, image만 담는 response 파일 적용

--- src/main/java/org/sopt/makers/internal/community/dto/response/PopularPostResponse.java
+++ src/main/java/org/sopt/makers/internal/community/dto/response/PopularPostResponse.java
@@ -1,21 +1,22 @@
 package org.sopt.makers.internal.community.dto.response;
 
 import org.sopt.makers.internal.community.domain.CommunityPost;
-import org.sopt.makers.internal.community.dto.MemberVo;
+import org.sopt.makers.internal.member.dto.response.MemberNameAndProfileImageResponse;
 
 public record PopularPostResponse(
         Long id,
         String category,
         String title,
-        MemberVo member,
+        MemberNameAndProfileImageResponse member,
         Integer hits
 ) {
     public static PopularPostResponse of(CommunityPost post, String categoryName) {
+
         return new PopularPostResponse(
                 post.getId(),
                 categoryName,
-                post.getTitle(),
-                MemberVo.of(post.getMember()),
+                title,
+                MemberNameAndProfileImageResponse.from(post.getMember()),
                 post.getHits()
         );
     }
