a75a158943b5f44bb7de88c931211ee4cbe487bc
임주민 <limj1213@gmail.com>
2025-05-27 19:01:06 +0900

    [Chore]: 가독성 향상을 위한 의미있는 메서드로 분리

--- src/main/java/org/sopt/makers/internal/community/service/post/CommunityPostService.java
+++ src/main/java/org/sopt/makers/internal/community/service/post/CommunityPostService.java
@@ -220,8 +220,8 @@ public class CommunityPostService {
     }
 
     @Transactional(readOnly = true)
-    public AnonymousPostProfile getAnonymousPostProfile(Long memberId, Long postId) {
-        return anonymousPostProfileRepository.findAnonymousPostProfileByMemberIdAndCommunityPostId(memberId, postId).orElse(null);
+    public AnonymousPostProfile getAnonymousPostProfile(Long postId) {
+        return anonymousPostProfileRepository.findAnonymousPostProfileByCommunityPostId(postId).orElse(null);
     }
 
     @Transactional(readOnly = true)
@@ -250,17 +250,8 @@ public class CommunityPostService {
             throw new BusinessLogicException("최근 한 달 내에 작성된 게시물이 없습니다.");
         }
 
-        List<Long> categoryIds = posts.stream()
-                .map(CommunityPost::getCategoryId)
-                .distinct()
-                .toList();
-        Map<Long, String> categoryNameMap = communityQueryRepository.getCategoryNamesByIds(categoryIds);
-
-        List<Long> postIds = posts.stream()
-                .map(CommunityPost::getId)
-                .distinct()
-                .toList();
-        Map<Long, AnonymousPostProfile> anonymousProfileMap = communityQueryRepository.getAnonymousPostProfilesByPostId(postIds);
+        Map<Long, String> categoryNameMap = getCategoryNameMap(posts);
+        Map<Long, AnonymousPostProfile> anonymousProfileMap = getAnonymousProfileMap(posts);
 
         return posts.stream()
                 .map(post -> communityResponseMapper.toPopularPostResponse(
@@ -399,5 +390,21 @@ public class CommunityPostService {
         if (!Objects.equals(memberId, postWriterId)) {
             throw new ClientBadRequestException("수정/삭제 권한이 없는 유저입니다.");
         }
+    }
+
+    private Map<Long, String> getCategoryNameMap(List<CommunityPost> posts) {
+        List<Long> categoryIds = posts.stream()
+                .map(CommunityPost::getCategoryId)
+                .distinct()
+                .toList();
+        return communityQueryRepository.getCategoryNamesByIds(categoryIds);
+    }
+
+    private Map<Long, AnonymousPostProfile> getAnonymousProfileMap(List<CommunityPost> posts) {
+        List<Long> postIds = posts.stream()
+                .map(CommunityPost::getId)
+                .distinct()
+                .toList();
+        return communityQueryRepository.getAnonymousPostProfilesByPostId(postIds);
     }
 }
