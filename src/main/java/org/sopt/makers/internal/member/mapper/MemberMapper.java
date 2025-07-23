82eccd3ad14b2b97ce342574abea102e3bed4591
임주민 <limj1213@gmail.com>
2025-07-23 23:58:04 +0900

    [fix] 테스트용 application.yml external 인증중앙화 관련 변수 추가

--- src/main/java/org/sopt/makers/internal/member/mapper/MemberMapper.java
+++ src/main/java/org/sopt/makers/internal/member/mapper/MemberMapper.java
@@ -4,11 +4,6 @@ import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.sopt.makers.internal.external.platform.InternalUserDetails;
-import org.sopt.makers.internal.internal.dto.InternalMemberActivityResponse;
-import org.sopt.makers.internal.internal.dto.InternalMemberProfileResponse;
-import org.sopt.makers.internal.internal.dto.InternalMemberProfileSpecificResponse;
-import org.sopt.makers.internal.internal.dto.InternalMemberResponse;
-import org.sopt.makers.internal.internal.dto.InternalOfficialMemberResponse;
 import org.sopt.makers.internal.member.domain.Member;
 import org.sopt.makers.internal.member.domain.MemberSoptActivity;
 import org.sopt.makers.internal.member.dto.ActivityVo;
@@ -20,15 +15,12 @@ import org.sopt.makers.internal.member.dto.response.MemberProfileSpecificRespons
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
-    InternalMemberResponse toInternalResponse(Member member, Integer latestGeneration);
     @Mapping(target = "name", source = "userDetails.name")
     @Mapping(target = "profileImage", source = "userDetails.profileImage")
     @Mapping(target = "birthday", source = "userDetails.birthday")
     @Mapping(target = "phone", source = "userDetails.phone")
     @Mapping(target = "email", source = "userDetails.email")
     MemberProfileResponse toProfileResponse (Member member, InternalUserDetails userDetails, Boolean isCoffeeChatActivate);
-
-    InternalMemberProfileResponse toInternalProfileResponse (Member member);
 
     @Mapping(target = "projects", source = "projects")
     MemberProfileProjectVo toSoptMemberProfileProjectVo(MemberSoptActivity member, List<MemberProjectVo> projects);
@@ -49,29 +41,12 @@ public interface MemberMapper {
             Boolean isCoffeeChatActivate
     );
 
-    @Mapping(target = "activities", source = "activities")
-    InternalMemberProfileSpecificResponse toInternalProfileSpecificResponse (
-            Member member,
-            boolean isMine,
-            List<MemberProfileProjectDao> projects,
-            List<InternalMemberProfileSpecificResponse.MemberActivityResponse> activities
-    );
-
-    @Mapping(target = "activities", source = "activities")
-    InternalMemberActivityResponse toInternalMemberActivityResponse (
-            Member member,
-            List<InternalMemberActivityResponse.MemberSoptActivityResponse> activities
-    );
-
     ActivityVo toActivityInfoVo (MemberSoptActivity activity, boolean isProject);
 
     @Mapping(source = "project.name", target = "team")
     ActivityVo toActivityInfoVo (MemberProfileProjectDao project, boolean isProject, String part);
 
     MemberProjectVo toActivityInfoVo (MemberProfileProjectDao project);
-
-    @Mapping(source = "generation", target = "generation")
-    InternalOfficialMemberResponse toOfficialResponse(Member member, String part, Integer generation);
 
     default String mapPhoneIfBlind(Boolean isPhoneBlind, String phone) {
         return isPhoneBlind ? null : phone;
