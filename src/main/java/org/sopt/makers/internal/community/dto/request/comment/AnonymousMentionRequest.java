6645251613224044a168e0c74a372a1cb99e86c7
dev-Crayon <seungheon328@gmail.com>
2025-10-30 16:01:37 +0900

    [Feat]: 커뮤니티 답글 생성 기능
    댓글 생성 API dto에 AnonymousMentionRequest 필드 추가
    멘션된 익명 닉네임 검증 및 푸시알림 발송을 위한 메서드
    
    Related to: #797

--- src/main/java/org/sopt/makers/internal/community/dto/request/comment/AnonymousMentionRequest.java
+++ src/main/java/org/sopt/makers/internal/community/dto/request/comment/AnonymousMentionRequest.java
@@ -1,5 +1,13 @@
 package org.sopt.makers.internal.community.dto.request.comment;
 
-public record AnonymousMentionRequest (
-    String[] anonymousNickname
-) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record AnonymousMentionRequest(
+
+        @Schema(
+                description = "익명 멘션된 사용자들의 익명 닉네임 배열",
+                example = "[\"오너십있는 츄러스\", \"도전하는 빙수\"]"
+        )
+        String[] anonymousNicknames
+) {
+}
