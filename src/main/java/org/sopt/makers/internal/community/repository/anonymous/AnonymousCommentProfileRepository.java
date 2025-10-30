9a8949745bae3049c4228da2fcc9f12d99632e6e
dev-Crayon <seungheon328@gmail.com>
2025-10-30 15:59:34 +0900

    [Feat]: 커뮤니티 답글 생성 기능
    AnonymousNicknameRepository: 리스트에 담긴 닉네임이 전부 DB에 존재하는 익명 닉네임인지 검증
    AnonymousCommentProfileRepository: 멘션된 익명 닉네임이 해당 게시글에서 전부 사용됐는지 검증
    
    Related to: #797

--- src/main/java/org/sopt/makers/internal/community/repository/anonymous/AnonymousCommentProfileRepository.java
+++ src/main/java/org/sopt/makers/internal/community/repository/anonymous/AnonymousCommentProfileRepository.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import org.sopt.makers.internal.community.domain.anonymous.AnonymousCommentProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AnonymousCommentProfileRepository extends JpaRepository<AnonymousCommentProfile, Long> {
 
@@ -13,4 +15,15 @@ public interface AnonymousCommentProfileRepository extends JpaRepository<Anonymo
 	Optional<AnonymousCommentProfile> findByMemberIdAndCommunityCommentPostId(Long memberId, Long postId);
 
 	Optional<AnonymousCommentProfile> findByCommunityCommentId(Long commentId);
+
+	@Query("""
+			SELECT DISTINCT acp.nickname.nickname
+			FROM AnonymousCommentProfile acp
+			WHERE acp.communityComment.postId = :postId
+			AND acp.nickname.nickname IN :nicknames
+			""")
+	List<String> findNicknamesByPostIdAndNicknamesIn(
+			@Param("postId") Long postId,
+			@Param("nicknames") List<String> nicknames
+	);
 }
