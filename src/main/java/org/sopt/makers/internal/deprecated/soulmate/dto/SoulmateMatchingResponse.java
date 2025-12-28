f43b99908f209a2f880a16e0070f7c9af84b1fc1
dev-Crayon <seungheon328@gmail.com>
2025-12-28 16:48:33 +0900

    [Refactor]: deprecated 기능 삭제
    소울메이트 & 광고
    
    Related to: #854

--- src/main/java/org/sopt/makers/internal/deprecated/soulmate/dto/SoulmateMatchingResponse.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.makers.internal.deprecated.soulmate.dto;
-
-import org.sopt.makers.internal.deprecated.soulmate.domain.SoulmateState;
-
-public record SoulmateMatchingResponse(
-        SoulmateState state,
-        Long soulmateId
-) {
-}
