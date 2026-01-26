4eebcc072d0be6c7a69459b2f31d2a5bbf2f99f9
dev-Crayon <seungheon328@gmail.com>
2026-01-26 13:27:31 +0900

    [Hotfix]: 가비아 문자 전송 오류

--- src/main/java/org/sopt/makers/internal/common/config/OkHttpConfig.java
+++ src/main/java/org/sopt/makers/internal/common/config/OkHttpConfig.java
@@ -2,33 +2,21 @@ package org.sopt.makers.internal.common.config;
 
 import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
-import okhttp3.TlsVersion;
-import okhttp3.CipherSuite;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
 
 @Configuration
 public class OkHttpConfig {
 
     @Bean
     public OkHttpClient gabiaOkHttpClient() {
-
-        ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-                .tlsVersions(TlsVersion.TLS_1_2)   // 일단 TLS 1.2로 고정
-                .cipherSuites(
-                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA
-                )
-                .build();
-
         return new OkHttpClient.Builder()
-                .connectionSpecs(List.of(spec))
+                .connectionSpecs(Arrays.asList(
+                        ConnectionSpec.MODERN_TLS,
+                        ConnectionSpec.COMPATIBLE_TLS
+                ))
                 .build();
     }
 }
