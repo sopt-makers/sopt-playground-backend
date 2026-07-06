dc0caab9db48f265b7e5353df860fd5fdcea0700
hyerinhwang-sailin <sallyh1111@ewhain.net>
2026-07-06 22:47:56 +0900

    fix: 음수 limit 요청 방어 처리

--- src/main/java/org/sopt/makers/internal/common/util/InfiniteScrollUtil.java
+++ src/main/java/org/sopt/makers/internal/common/util/InfiniteScrollUtil.java
@@ -1,6 +1,6 @@
 package org.sopt.makers.internal.common.util;
 
-import lombok.val;
+import org.sopt.makers.internal.exception.BadRequestException;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -9,8 +9,15 @@ import java.util.List;
 @Component
 public class InfiniteScrollUtil {
     public Integer checkLimitForPagination(Integer limit) {
-        val isLimitEmpty = (limit == null || limit <= 0);
-        return isLimitEmpty ? null : limit + 1;
+        if (limit == null || limit == 0) {
+            return null;
+        }
+
+        if (limit < 0) {
+            throw new BadRequestException("limit은 0 이상이어야 합니다.");
+        }
+
+        return limit + 1;
     }
 
     public <T> Boolean checkHasNextElement(Integer limit, List<T> elementList) {
